24f435ac21ad887aad0d8df706cee1982a1811f6
Noel Cothren <noelcothren@gmail.com>
2017-01-24 13:53:29 -0700

    added details to Margie's persona

--- epic/persona-margie.php
+++ epic/persona-margie.php
@@ -2,15 +2,15 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Persona 1: Noah</title>
+		<title>Persona 2: Margie</title>
 	</head>
 	<body>
-		<h1>Persona</h1>
-		<p>Name: </p>
-		<p>Age: </p>
-		<p>Gender/Sex: </p>
-		<p>Occupation: </p>
-		<p>Technology: </p>
+		<h1>Persona 2</h1>
+		<p>Name: Margie</p>
+		<p>Age: 62 </p>
+		<p>Gender/Sex: Female </p>
+		<p>Occupation: retired teacher and hobby farmer </p>
+		<p>Technology: Margie has a desktop PC running Windows 2000 and uses Internet Explorer as her browser. </p>
 		<p>Needs: </p>
 		<p>Goals: </p>
 	</body>
