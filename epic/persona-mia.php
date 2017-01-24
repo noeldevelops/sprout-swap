3bae2b0f1975ae3d09c5ccad70a95a9a63ee94c5
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-24 14:00:42 -0700

    filling info for mia's persona

--- epic/persona-mia.php
+++ epic/persona-mia.php
@@ -5,13 +5,13 @@
 		<title>Persona 1: Noah</title>
 	</head>
 	<body>
-		<h1>Persona</h1>
-		<p>Name: </p>
-		<p>Age: </p>
-		<p>Gender/Sex: </p>
-		<p>Occupation: </p>
-		<p>Technology: </p>
-		<p>Needs: </p>
+		<h1>First Persona</h1>
+		<p>Name: Mia</p>
+		<p>Age:27</p>
+		<p>Gender/Sex: Female</p>
+		<p>Occupation: Marketing Advisor</p>
+		<p>Technology: galaxy note 7 and a iMac desktop, and macbookpro.</p>
+		<p>Needs: She enjoys the urban lifestyle, but likes to grow her own vegatables from her apartment. But her living lifestyle is small, so she can only grow 2 or 3 vegatables at a time. So she wants to swap her vegatables with someone that has a bigger varity then her.</p>
 		<p>Goals: </p>
 	</body>
 </html>
