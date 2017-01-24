4cec980ec2ed1c419446e41fa32e2c6a5e8a68ca
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-24 15:30:39 -0700

    use case for mia

--- epic/use-case-mia.php
+++ epic/use-case-mia.php
@@ -3,6 +3,13 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Use Case 3: Mia</title>
+		<h1>Mia's use case</h1>
+		<ol>
+			<li>Mia gets on her iMac</li>
+			<li>Mia searches for sprout-swap</li>
+			<li>Mia makes a post on sprout-swap if anyone wants to trade cabbage and carrots in her local area.</li>
+			<li>Mia comes back later on her iMac to see if she has notifications from anyone on sprout-swap.</li>
+		</ol>
 	</head>
 	<body>
 
