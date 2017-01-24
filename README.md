c987365424f9c26b893446f9a7d3cf510f814462
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-24 13:33:48 -0700

    developed Noah's persona

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sprout-swap
 Capstone
+
+An online system which allows local growers to connect with people in the area interested in buying/swapping for their homegrown vegetables.  Users with profiles may make posts with images offering items from their gardens and other users, with or without profiles, may respond and offer to trade or buy these yummy veggies.
---
+++ epic/persona-noah.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+			<p>Name: Noah</p>
+			<p>Age: 23</p>
+			<p>Gender/Sex: Male</p>
+			<p>Occupation: Grad Student at UNM, works in the bike shop</p>
+			<p>Technology: Noah owns an iPhone 6 and a 2012 Macbook Pro running OSX.  He always has his cell phone on him but usually leaves his laptop at home.  He uses Safari on his iPhone and Chrome on his laptop.</p>
+			<p>Needs: Noah is looking for cheap homegrown vegetables however, the local farmer's market is only open twice a month.  He would rather get in contact directly with local growers to see what they have to offer.</p>
+			<p>Goals: </p>
+	</body>
+</html>
