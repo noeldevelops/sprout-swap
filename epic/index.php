1c68fdf091047f619be3e0f102e610e0b6044ac2
Noel Cothren <noelcothren@gmail.com>
2017-01-24 16:22:29 -0700

    added some div tags for more readability

--- epic/index.php
+++ epic/index.php
@@ -7,12 +7,21 @@
 	<body>
 		<h1>Executive Summary: Sprout Swap</h1>
 		<p>Sprout Swap is a dynamic website that offers hobby farmers and growers the opportunity to get rid of excess produce by trading, bartering, or selling. Users can create a profile, make posts with text and images of produce they have to offer, and view a feed of others' offerings that can be filtered by location or neighborhood. They will be able to send private messages to other site users and respond to postings. All responsibility for food safety and financial exchanges will be done privately between users, with no liability assumed by Sprout Swap. </p>
-		<a href="use-case-noah.php">Noah Persona</a><br/>
-		<a href="use-case-noah.php">Noah Use Case</a><br/>
-		<a href="persona-margie.php">Margie Persona</a><br/>
-		<a href="use-case-margie.php">Margie Use Case</a><br/>
-		<a href="persona-mia.php">Mia Persona</a><br/>
-		<a href="use-case-mia.php">Mia Use Case</a><br/>
+		<div>
+			<h1>Use Case 1: Noah</h1>
+			<a href="use-case-noah.php">Noah Persona</a><br/>
+			<a href="use-case-noah.php">Noah Use Case</a><br/>
+		</div>
+		<div>
+			<h1>Use Case 2: Margie</h1>
+			<a href="persona-margie.php">Margie Persona</a><br/>
+			<a href="use-case-margie.php">Margie Use Case</a><br/>
+		</div>
+		<div>
+			<h1>Use Case 3: Mia</h1>
+			<a href="persona-mia.php">Mia Persona</a><br/>
+			<a href="use-case-mia.php">Mia Use Case</a><br/>
+		</div>
 		<a href="interaction-flows.php">Interaction Flows</a><br/>
 		<h1>User Stories</h1>
 			<p>As a guest user, I want to be able to browse for produce in my area and find the poster's given contact information</p>
