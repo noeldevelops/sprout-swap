7c77fa9b3117f5b40ba60a7dcb8849f5cef8f6ed
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-24 16:03:14 -0700

    added line breaks to index.php

--- epic/index.php
+++ epic/index.php
@@ -7,12 +7,12 @@
 	<body>
 		<h1>Executive Summary: Sprout Swap</h1>
 		<p>Sprout Swap is a dynamic website that offers hobby farmers and growers the opportunity to get rid of excess produce by trading, bartering, or selling. Users can create a profile, make posts with text and images of produce they have to offer, and view a feed of others' offerings that can be filtered by location or neighborhood. They will be able to send private messages to other site users and respond to postings. All responsibility for food safety and financial exchanges will be done privately between users, with no liability assumed by Sprout Swap. </p>
-		<a href="use-case-noah.php">Noah Persona</a>
-		<a href="use-case-noah.php">Noah Use Case</a>
-		<a href="persona-margie.php">Margie Persona</a>
-		<a href="use-case-margie.php">Margie Use Case</a>
-		<a href="persona-mia.php">Mia Persona</a>
-		<a href="use-case-mia.php">Mia Use Case</a>
-		<a href="interaction-flows.php">Interaction Flows</a>
+		<a href="use-case-noah.php">Noah Persona<br/></a>
+		<a href="use-case-noah.php">Noah Use Case<br/></a>
+		<a href="persona-margie.php">Margie Persona<br/></a>
+		<a href="use-case-margie.php">Margie Use Case<br/></a>
+		<a href="persona-mia.php">Mia Persona<br/></a>
+		<a href="use-case-mia.php">Mia Use Case<br/></a>
+		<a href="interaction-flows.php">Interaction Flows<br/></a>
 	</body>
 </html>
