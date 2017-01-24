c4994e6d6578e936b838c48aa42282ebb6937139
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-24 13:49:27 -0700

    added boilerplate to mia and margie personas

--- epic/persona-mia.php
+++ epic/persona-mia.php
@@ -2,9 +2,16 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title></title>
+		<title>Persona 1: Noah</title>
 	</head>
 	<body>
-
+		<h1>Persona</h1>
+		<p>Name: </p>
+		<p>Age: </p>
+		<p>Gender/Sex: </p>
+		<p>Occupation: </p>
+		<p>Technology: </p>
+		<p>Needs: </p>
+		<p>Goals: </p>
 	</body>
 </html>
