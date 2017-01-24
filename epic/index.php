98c66292810966b92c25861d86d94fc0763f8b8f
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-24 15:54:05 -0700

    added links to other files in index.php

--- epic/index.php
+++ epic/index.php
@@ -5,6 +5,12 @@
 		<title>index.php</title>
 	</head>
 	<body>
-
+		<a href="use-case-noah.php">Noah Persona</a>
+		<a href="use-case-noah.php">Noah Use Case</a>
+		<a href="persona-margie.php">Margie Persona</a>
+		<a href="use-case-margie.php">Margie Use Case</a>
+		<a href="persona-mia.php">Mia Persona</a>
+		<a href="use-case-mia.php">Mia Use Case</a>
+		<a href="interaction-flows.php">Interaction Flows</a>
 	</body>
 </html>
