7a02108e30305eb1acb95499fbf6c2f6000d836f
Noel Cothren <noelcothren@gmail.com>
2017-01-24 15:23:02 -0700

    adding details to Use Case scenario for user persona Margie

--- epic/use-case-margie.php
+++ epic/use-case-margie.php
@@ -6,6 +6,6 @@
 	</head>
 	<body>
 		<h1>Use Case: Margie</h1>
-		<p>Margie heard about Sprout Swap from a fellow local grower and is eager to try it out. She sits at her home computer after her morning chores, opens IE, navigates to the URL, and clicks on the "Sign Up" link. She then has to verify her email address through the link that is sent to her, and once back on the site she types in some information about herself in the description and uploads a photo.</p>
+		<p>Margie heard about Sprout Swap from a fellow local grower and is eager to try it out. She sits at her home computer after her morning chores, opens IE, navigates to the URL, and clicks on the "Sign Up" link. She then has to verify her email address through the link that is sent to her, and once back on the site she types in some information about herself in the description and uploads a photo of her in her garden.</p>
 	</body>
 </html>
