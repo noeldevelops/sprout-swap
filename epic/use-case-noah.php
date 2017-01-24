bcea289b1c12d181efcf14be3748b1e74c9765e6
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-24 15:21:03 -0700

    added use case for noah

---
+++ epic/use-case-noah.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Use Case: Noah</title>
+	</head>
+	<body>
+		<h1>Use Case: Noah</h1>
+			<ol>
+				<li>While at work, Noah opens sprout-swap on his iPhone</li>
+				<li>He scrolls through the Albuquerque feed, then limits his feed to a handful of neighborhoods that are on his way home</li>
+				<li>Noticing a post for cheap squash, he clicks on the post which opens and offers more information</li>
+				<li>Noah clicks the "contact" button on the bottom of the post which opens a new message with the pre-populated phone number given by the poster</li>
+			</ol>
+	</body>
+</html>
