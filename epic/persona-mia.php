7988ff5e3f42c364cc13eb18a3b48c9eb66348eb
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-24 14:57:45 -0700

    fixed typos

--- epic/persona-mia.php
+++ epic/persona-mia.php
@@ -9,9 +9,9 @@
 		<p>Name: Mia</p>
 		<p>Age:27</p>
 		<p>Gender/Sex: Female</p>
-		<p>Occupation: Marketing Advisor</p>
-		<p>Technology: galaxy note 7 and a iMac desktop, and macbookpro.</p>
-		<p>Needs: She enjoys the urban lifestyle, but likes to grow her own vegatables from her apartment. But her living lifestyle is small, so she can only grow 2 or 3 vegatables at a time. So she wants to swap her vegatables with someone that has a bigger varity then her.</p>
+		<p>Occupation: Marketing Adviser</p>
+		<p>Technology: galaxy note 7 and a iMac desktop, and Macbook Pro.</p>
+		<p>Needs: She enjoys the urban lifestyle, but likes to grow her own vegetables from her apartment. But her living lifestyle is small, so she can only grow 2 or 3 vegetables at a time. So she wants to swap her vegetables with someone that has a bigger variety then her.</p>
 		<p>Goals: </p>
 	</body>
 </html>
