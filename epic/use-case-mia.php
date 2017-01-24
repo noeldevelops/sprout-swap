db8fcde8bd060301efa6b95ebadeed87deaa1c83
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-24 15:39:52 -0700

    added to mia's use case

--- epic/use-case-mia.php
+++ epic/use-case-mia.php
@@ -11,7 +11,7 @@
 			<li>Mia searches for sprout-swap</li>
 			<li>Mia makes a post on sprout-swap if anyone wants to trade cabbage and carrots in her local area.</li>
 			<li>Mia comes back later on her iMac to see if she has notifications from anyone on sprout-swap.</li>
-			<li>Mia opens up a message and replys back to the other user</li>
+			<li>Mia opens up a message and replys back and forth with the other user, making a set place and time for the swap.</li>
 		</ol>
 	</body>
 </html>
