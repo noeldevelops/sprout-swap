d9219c75ca04710c6d82631c7053bf5baa6f6150
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-24 13:37:19 -0700

    finished Noah's persona

--- epic/persona-noah.php
+++ epic/persona-noah.php
@@ -5,13 +5,13 @@
 		<title></title>
 	</head>
 	<body>
-		<h1>Persona</h1>
+		<h1>First Persona</h1>
 			<p>Name: Noah</p>
 			<p>Age: 23</p>
 			<p>Gender/Sex: Male</p>
 			<p>Occupation: Grad Student at UNM, works in the bike shop</p>
 			<p>Technology: Noah owns an iPhone 6 and a 2012 Macbook Pro running OSX.  He always has his cell phone on him but usually leaves his laptop at home.  He uses Safari on his iPhone and Chrome on his laptop.</p>
 			<p>Needs: Noah is looking for cheap homegrown vegetables however, the local farmer's market is only open twice a month.  He would rather get in contact directly with local growers to see what they have to offer.</p>
-			<p>Goals: </p>
+			<p>Goals: On sprout-swap Noah is able to browse through posts made by growers and filter them by proximity to his location.  This allows him to directly contact and schedule to meet with growers offering the vegetables/compost/other gardening things he needs rather than having to drive/bike all the way to the grower's market.</p>
 	</body>
 </html>
