8afac1117e40e1be2ca5771fbe7bf4fe6abeb814
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-24 15:34:54 -0700

    added interaction flow for noah

---
+++ epic/interaction-flows.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Use Case: Noah</title>
+	</head>
+	<body>
+		<h1>Interaction Flow: Noah</h1>
+		<ol>
+			<li>While at work, Noah opens sprout-swap on his iPhone</li>
+			<li>sprout-swap's server receives the request and notices Noah is on a mobile device.  the server sends the appropriate files to Noah's phone browser</li>
+			<li>the server sees noah is located in Albuquerque and loads the Albuquerque feed</li>
+			<li>Noah scrolls through the Albuquerque feed, then limits his feed to a handful of neighborhoods that are on his way home</li>
+			<li>the server limits the posts which show up on his feed based on Abq neighborhood ID</li>
+			<li>Noticing a post for cheap squash, Noah clicks on the post</li>
+			<li> The server opens the post link which offers more information including the requested price(money or barter), name of the grower, etc...</li>
+			<li>Noah clicks the "contact" button on the bottom of the post</li>
+			<li>the server sends a message populated with the poster's provided contact information and opens a new message on his phone</li>
+		</ol>
+	</body>
+</html>
--- epic/use-case-noah.php
+++ epic/use-case-noah.php
@@ -9,7 +9,7 @@
 			<ol>
 				<li>While at work, Noah opens sprout-swap on his iPhone</li>
 				<li>He scrolls through the Albuquerque feed, then limits his feed to a handful of neighborhoods that are on his way home</li>
-				<li>Noticing a post for cheap squash, he clicks on the post which opens and offers more information</li>
+				<li>Noticing a post for cheap squash, he clicks on the post which opens and offers more information including the requested price(money or barter), name of the grower, etc...</li>
 				<li>Noah clicks the "contact" button on the bottom of the post which opens a new message with the pre-populated phone number given by the poster</li>
 			</ol>
 	</body>
