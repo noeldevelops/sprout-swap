4a51e5145a9d9a4942b8e5fd360ef9bc399c012c
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-24 15:04:07 -0700

    finished mia's persona

--- epic/persona-mia.php
+++ epic/persona-mia.php
@@ -2,16 +2,16 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Persona 1: Noah</title>
+		<title>Persona 3: Mia</title>
 	</head>
 	<body>
-		<h1>First Persona</h1>
+		<h1>Thrid Persona</h1>
 		<p>Name: Mia</p>
 		<p>Age:27</p>
 		<p>Gender/Sex: Female</p>
 		<p>Occupation: Marketing Adviser</p>
-		<p>Technology: galaxy note 7 and a iMac desktop, and Macbook Pro.</p>
+		<p>Technology: galaxy note 7, a iMac desktop, and Macbook Pro.</p>
 		<p>Needs: She enjoys the urban lifestyle, but likes to grow her own vegetables from her apartment. Because her living space is small she can only grow 2 or 3 vegetables at a time. So she wants to swap her vegetables with someone that has a bigger variety then her.</p>
-		<p>Goals: </p>
+		<p>Goals: To be able to connect with others that are growing their own vegatables, so she will be able to trade with others that are growing something that she doesn't have.</p>
 	</body>
 </html>
