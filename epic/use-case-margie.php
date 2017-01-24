deb150ae893662cd7d5587752d9a8881a483964e
Noel Cothren <noelcothren@gmail.com>
2017-01-24 15:20:15 -0700

    complete the Use Case scenario for user persona Margie

--- epic/use-case-margie.php
+++ epic/use-case-margie.php
@@ -5,6 +5,7 @@
 		<title>Use Case 2: Margie</title>
 	</head>
 	<body>
-
+		<h1>Use Case: Margie</h1>
+		<p>Margie heard about Sprout Swap from a fellow local grower and is eager to try it out. She sits at her home computer after her morning chores, opens IE, navigates to the URL, and clicks on the "Sign Up" link. She then has to verify her email address through the link that is sent to her, and once back on the site she types in some information about herself in the description and uploads a photo.</p>
 	</body>
 </html>
