dcef960873af9cb644d940e1acab5ed4c440a4ba
Noel Cothren <noelcothren@gmail.com>
2017-01-24 16:07:36 -0700

    more descriptive title

--- epic/index.php
+++ epic/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>index.php</title>
+		<title>Index and Executive Summary</title>
 	</head>
 	<body>
 		<h1>Executive Summary: Sprout Swap</h1>
