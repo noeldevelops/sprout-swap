f76ac2d86e7d9e45511bdc9f4781d66800b3ab40
Noel Cothren <noelcothren@gmail.com>
2017-01-24 14:00:35 -0700

    added more details to Margie's persona

--- epic/persona-margie.php
+++ epic/persona-margie.php
@@ -10,8 +10,8 @@
 		<p>Age: 62 </p>
 		<p>Gender/Sex: Female </p>
 		<p>Occupation: retired teacher and hobby farmer </p>
-		<p>Technology: Margie has a desktop PC running Windows 2000 and uses Internet Explorer as her browser. </p>
-		<p>Needs: </p>
-		<p>Goals: </p>
+		<p>Technology: Margie has a desktop PC running Windows 2000 and uses Internet Explorer as her browser. She does not have a smart phone, but has a landline at home and an internet connection through Century Link. She is not very tech-savvy. </p>
+		<p>Needs: Margie is a very good grower and often has way more produce than she and her husband can eat. Even though she doesn't want to make a profit off her vegetables, she doesn't want good food to go to waste and needs a way to offer her produce to others in her community. </p>
+		<p>Goals: She wants a simple way to let others know what produce she has to offer and get messages and make plans to trade with others. </p>
 	</body>
 </html>
