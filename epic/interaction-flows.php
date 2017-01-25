b1dc51567fde4b569d45bf5478da8a10c9b860aa
Noel Cothren <noelcothren@gmail.com>
2017-01-25 16:12:26 -0700

    fixed some typos

--- epic/interaction-flows.php
+++ epic/interaction-flows.php
@@ -34,9 +34,9 @@
 			<li>When Mia is home she gets on her iMac and opens up google chrome</li>
 			<li>Mia searches for sprout-swap on the search bar.</li>
 			<li>Mia sees the website and opens up the website</li>
-			<li>Mia gets on her profile and post some vegatables are her wall/feed.</li>
-			<li>Mia leaves her iMac to do something and plans on coming back shortly to see if she has any replys.</li>
-			<li>Then she comes back to her iMac to see if she has any reply and discoverys she has a reply</li>
+			<li>Mia gets on her profile and post some vegetables are her wall/feed.</li>
+			<li>Mia leaves her iMac to do something and plans on coming back shortly to see if she has any replies.</li>
+			<li>Then she comes back to her iMac to see if she has any reply and discoveries she has a reply</li>
 			<li>She replys back to the other user and to see what would be the best place and time they can meet. Mia told her what are the best times for herself.</li>
 			<li>Mia waits to see if she has a quick reply. Which she does and then begins to plan </li>
 		</ol>
