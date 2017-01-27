037d7d1b5f54a6e49da163528a8bfee46ca5a415
Noel Cothren <noelcothren@gmail.com>
2017-01-27 12:13:08 -0700

    create tables for profile and post
    https://app.asana.com/0/256424297438823/256424297438827

---
+++ sql/sprout-swap.sql
@@ -0,0 +1,34 @@
+-- these statements will drop the tables and re-add them
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS message;
+DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS mode;
+
+CREATE TABLE profile (
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileActivation CHAR,
+	profileEmail VARCHAR NOT NULL,
+	profileHandle VARCHAR NOT NULL,
+	profileImageId INT,
+	profileJoinDate DATETIME,
+	profileName VARCHAR,
+	profilePasswordHash CHAR,
+	profileSalt CHAR,
+	profileSummary VARCHAR,
+
+	PRIMARY KEY (profileId)
+);
+
+CREATE TABLE post (
+	postId INT,
+	postModeId,
+	postProfileId,
+	postContent,
+	postDateTime,
+	postLocation,
+	postOffer VARCHAR,
+	postRequest,
+);
+
+CREATE TABLE
