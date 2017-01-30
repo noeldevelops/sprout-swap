4958c3663216984524e701f9575389d727fb79dc
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-30 15:50:37 -0700

    added profileId and profileImageId classes
    https://app.asana.com/0/256424297438823/257850751954520

---
+++ classes/Profile.php
@@ -0,0 +1,14 @@
+<?php
+
+class Profile{
+	/**
+	 * id for the profile; this is a primary key
+	 * @var int for profileId
+	 **/
+	private $profileId;
+	/**
+	 * id for profileImageId
+	 * this is a foreign key that references image (imageId)
+	 * @var int $profileImageId
+	 **/
+}	private $profileImageId;
