fa389ed5d74b661fe90bb891a8bbc6df7e9c42ca
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-30 15:50:55 -0700

    added constructor function to Message.php
    https://app.asana.com/0/256424297438823/257850751954522

--- classes/Message.php
+++ classes/Message.php
@@ -47,5 +47,25 @@ class Message{
 	 */
 	private $messageTimestamp;
 
-
+	public function __construct(int $newMessageId = null, int $newMessagePostId, int $newMessageReceiverProfileId, int $newMessageSenderProfileId, string $newMessageBrowser, string $newMessageContent,  $newMessageIpAddress, int $newMessageStatus, $newMessageTimestamp = null) {
+		try{
+			$this->setMessageId($newMessageId);
+			$this->setMessagePostId($newMessagePostId);
+			$this->setMessageReceiverProfileId($newMessageReceiverProfileId);
+			$this->setMessageSenderProfileId($newMessageSenderProfileId);
+			$this->setMessageBrowser($newMessageBrowser);
+			$this->setMessageContent($newMessageContent);
+			$this->setMessageIpAddress($newMessageIpAddress);
+			$this->setMessageStatus($newMessageStatus);
+			$this->setMessageTimestamp($newMessageTimestamp);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $rangeException) {
+			throw (new \RangeException($rangeException->getMessage(), 0, $rangeException));
+		} catch(\TypeError $typeError) {
+			throw (new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			throw (new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
 }
