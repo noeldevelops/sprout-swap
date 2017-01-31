9499f1b642b5a4fcaea096fd19fb90bdfab122a0
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-31 11:17:29 -0700

    added PostImage constructor
    https://app.asana.com/0/256424297438823/257850751954527

--- classes/PostImage.php
+++ classes/PostImage.php
@@ -2,8 +2,28 @@
 
 class PostImage implements \JsonSerializable{
 	/**
-	 * @var
+	 * foreign key
+	 * @var int $postImageImageId
 	 */
-	private $postPostImageId;
-	Private $postImagePostId;
+	private $postImageImageId;
+	/**
+	 * foreign key
+	 * @var int $postImagePostId
+	 */
+	private $postImagePostId;
+
+	public function __construct(int $postImageImageId, int $postImagePostId) {
+		try{
+			$this->setPostImageImageId($postImageImageId);
+			$this->setPostImagePostId($postImagePostId);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $rangeException) {
+			throw (new \RangeException($rangeException->getMessage(), 0, $rangeException));
+		} catch(\TypeError $typeError) {
+			throw (new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			throw (new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
 }
