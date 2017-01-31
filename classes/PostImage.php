8a49a6ef0944dd45715d5566d890d209dd6c2fde
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-31 15:39:05 -0700

    added insert, update, and delete methods; updated commenting
    https://app.asana.com/0/256424297438823/257850751954527

--- classes/PostImage.php
+++ classes/PostImage.php
@@ -1,5 +1,4 @@
 <?PHP
-
 class PostImage implements \JsonSerializable{
 	/**
 	 * foreign key
@@ -11,11 +10,19 @@ class PostImage implements \JsonSerializable{
 	 * @var int $postImagePostId
 	 */
 	private $postImagePostId;
-
-	public function __construct(int $postImageImageId, int $postImagePostId) {
+	/**
+	 * PostImage constructor.
+	 * @param int $newPostImageImageId
+	 * @param int $newPostImagePostId
+	 * @throws \Exception if some error occurs
+	 * @throws \TypeError if data is in incorrect formats
+	 * @throws \InvalidArgumentException if invalid data
+	 * @throws \RangeException if data outside of bounds
+	 */
+	public function __construct(int $newPostImageImageId, int $newPostImagePostId) {
 		try{
-			$this->setPostImageImageId($postImageImageId);
-			$this->setPostImagePostId($postImagePostId);
+			$this->setPostImageImageId($newPostImageImageId);
+			$this->setPostImagePostId($newPostImagePostId);
 		} catch(\InvalidArgumentException $invalidArgument) {
 			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
 		} catch(\RangeException $rangeException) {
@@ -26,7 +33,6 @@ class PostImage implements \JsonSerializable{
 			throw (new \Exception($exception->getMessage(), 0, $exception));
 		}
 	}
-
 	/**
 	 * accessor method for PostImageImageId
 	 * @return int
@@ -36,14 +42,14 @@ class PostImage implements \JsonSerializable{
 	}
 	/**
 	 * mutator method for postImageImageId
-	 * @param int $postImageImageId
+	 * @param int $newPostImageImageId
 	 * @throws \RangeException if postImageImageId is less than or equal to zero
 	 */
-	public function setPostImageImageId(int $postImageImageId) {
+	public function setPostImageImageId(int $newPostImageImageId) {
 		if($this->postImageImageId >= 0){
 			throw(new \RangeException("postImageImageId must be greater than zero"));
 		}
-		$this->postImageImageId = $postImageImageId;
+		$this->postImageImageId = $newPostImageImageId;
 	}
 
 	/**
@@ -54,17 +60,57 @@ class PostImage implements \JsonSerializable{
 		return $this->postImagePostId;
 	}
 	/**
-	 * @param int $postImagePostId
+	 * @param int $newPostImagePostId
 	 * @throws RangeException if postImagePostId is less than or equal to zero
 	 */
-	public function setPostImagePostId(int $postImagePostId){
+	public function setPostImagePostId(int $newPostImagePostId){
 		if($this->postImagePostId >= 0){
 			throw(new \RangeException("postImageImageId must be greater than zero"));
 		}
-		$this->postImagePostId = $postImagePostId;
+		$this->postImagePostId = $newPostImagePostId;
 	}
+	/**
+	 * JSON Serializable method
+	 * @return array of data to be serialized
+	 */
 	public function jsonSerialize(){
 		$fields = get_object_vars($this);
 		return($fields);
+	}
+	/**
+	 * Insert function
+	 * @param PDO $pdo PHP data connection object
+	 */
+	public function insert(\PDO $pdo){
+		//create query template
+		$query = "INSERT INTO postImage(postImageImageId, postImagePostId) VALUES(:postImageImageId, :postImagePostId)";
+		$statement = $pdo->prepare($query);
+		//bind variables and execute
+		$parameters = ["postImageImageId" => $this->postImageImageId, "postImagePostId" => $this->postImagePostId];
+		$statement->execute($parameters);
+	}
+	/**
+	 * Delete function
+	 * @param PDO $pdo PHP data connection object
+	 */
+	public function delete(\PDO $pdo){
+		//create query template
+		$query = "DELETE FROM postImage WHERE postImageImageId = :postImageImageId AND postImagePostId = :postImagePostId";
+		$statement = $pdo->prepare($query);
+		//bind variables
+		$parameters = ["postImageImageId" => $this->postImageImageId, "postImagePostId" => $this->postImagePostId];
+		$statement->execute($parameters);
+	}
+	/**
+	 * Update function
+	 * @param PDO $pdo PHP data connection object
+	 */
+	public function update(\PDO $pdo){
+		//create query template
+		$query = "UPDATE postImage SET postImageImageId = :postImageImageId, postImagePostId= :postImagePostId";
+		$statement = $pdo->prepare($query);
+		//bind variables
+		$parameters = ["postImageImageId" => $this->postImageImageId, "postImagePostId" => $this->postImagePostId];
+		$statement->execute($parameters);
 	}
 }
