85a2d499248d8e83072116c7ea4b3df7e50f5617
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-02-01 16:50:20 -0700

    finished public static function for getProfileByProfileActivation
    https://app.asana.com/0/256424297438823/257850751954520

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -484,5 +484,42 @@ SELECT profileId, profileImageId, profileActivation, profileEmail, profileHandle
 		 **/
 	}
 
-	public static function getProfileByProfileActivation
+	public static function getProfileByProfileActivation(\PDO $pdo, string $profileActivation){
+		$profileActivation = trim($profileActivation);
+		$profileActivation = filter_var($profileActivation, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($profileActivation) === true){
+			throw(new \PDOException("profile activation is invalid"));
+		}
+
+		$query = "SELECT profileId, profileImageId, profileActivation, profileEmail, profileHandle, profileTimestamp, profileName, profilePasswordHash, profileSalt, profileSummary FROM profile WHERE profileId = :profileId";
+		$statement = $pdo->prepare($query);
+
+		$profileActivation = "%$profileActivation%";
+		$parameters = ["profileActivation" => $profileActivation];
+		$statement->execute($parameters);
+
+		$profileActivation = new \SplFixedArray(($statement->rowCount()));
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch() !== false));
+			try {
+				$profileActivation = new Profile($row["profileId"], $row["profileImageId"], $row ["profileActivation"], $row["profileEmail"], $row["profileHandle"], $row["profileTimestamp"], $row["profileName"], $row["profilePasswordHash"], $row["profileSalt"], $row["profileSummary"]);
+				$profileActivation[$profileActivation->key()] = $profileActivation;
+				$profileActivation->next();
+			}catch(\Exception $exception){
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+			return($profileActivation);
+
+			/**
+			 * get the profile activation by profile
+			 *
+			 * @param \PDO $pdo PDO conncetion object
+			 * @param int $profileActivation profile activation to search for
+			 * @return Profile|null profile activation or null if not found
+			 * @throws \PDOException when mySQL related errors occur
+			 * @throws \TypeError when variables are not the correct data type
+			 **/
+
+
+	}
 }
