89b54d25110da9e2b23050d6a9b4600cfa8ec54e
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-01 16:42:50 -0700

    added insertValidMessage test
    https://app.asana.com/0/256424297438823/257850751954523

--- php/classes/test/MessageTest.php
+++ php/classes/test/MessageTest.php
@@ -27,7 +27,8 @@ class messageTest extends DataDesignTest{
 	public final function setUp(){
 		parent::setUp();
 		$this->image = new Image(null, "sjnghsklguenghtls");
-		$this->profile = new Profile(null, null, "kjsdhkj", "solomon.leyba@gmail.com", "ranch.me", null, "Solomon Leyba", "803AE81D0D6F67C1C0D307B39A99A93F6B6499B4C6E3F2ECE96718C5E2724B96", "5A929D9C14C5DF68BD2C97BBE2652754E26B3C9D23AC91978A0B9C0EAA3DE347", "god damn i love unit testing");
+		$this->receiverProfile = new Profile(null, null, "kjsdhkj", "solomon.leyba@gmail.com", "ranch.me", null, "Solomon Leyba", "803AE81D0D6F67C1C0D307B39A99A93F6B6499B4C6E3F2ECE96718C5E2724B96", "5A929D9C14C5DF68BD2C97BBE2652754E26B3C9D23AC91978A0B9C0EAA3DE347", "god damn i love unit testing");
+		$this->senderProfile = new Profile(null, null, "sdfsd", "djt@america.gov", "hello_us", null, "Noel Cothren", "9BB789D2052F1E787C89A700A59EF22DE1AFAEACC0E2DE97D22DC1D04284E871", "4C703B281FB196C94B61CC075B1F3191A0D9A4CEE2A46E153449728D3EC18503", "god damn i STILL love unit testing");
 		$this->VALID_MESSAGETIMESTAMP = new \DateTime();
 
 	}
@@ -35,6 +36,18 @@ class messageTest extends DataDesignTest{
 		//store number of current rows to compare against
 		$numRows = $this->getConnection()->getRowCount("message");
 		//create new message and insert
-		$this->message = new Message(null, );
+		$message = new Message(null, $this->receiverProfile->profileId, $this->senderProfile->profileId, $this->VALID_MESSAGEBROWSER, $this->VALID_MESSAGECONTENT, $this->VALID_MESSAGEIPADDRESS, $this->VALID_MESSAGESTATUS, $this->VALID_MESSAGETIMESTAMP);
+
+		$pdoMessage = Message::getMessageByMessageId($this->getPDO(), $message->getMessageId());
+		$this->assertEquals($numRows + 1, $this->getConnection->getRowCount("message"));
+		$this->assertEquals($pdoMessage->getMessageId(), $this->getMessageId());
+		$this->assertEquals($pdoMessage->getMessageReceiverProfileId(), $this->profile->getMessageReceiverProfileId());
+		$this->assertEquals($pdoMessage->getMessageSenderProfileId(), $this->profile->getMessageSenderProfileId());
+		$this->assertEquals($pdoMessage->getMessageBrowser(), $this->VALID_MESSAGEBROWSER);
+		$this->assertEquals($pdoMessage->getMessageContent(), $this->VALID_MESSAGECONTENT);
+		$this->assertEquals($pdoMessage->getMessageIpAddress(), $this->VALID_MESSAGEIPADDRESS);
+		$this->assertEquals($pdoMessage->getMessageStatus(), $this->VALID_MESSAGESTATUS);
+		$this->assertEquals($pdoMessage->getMessageTimestamp(), $this->VALID_MESSAGEIPADDRESS);
+
 	}
 }
