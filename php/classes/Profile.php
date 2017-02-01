93aa2b50dd7dc274bd706e2348f275422baec602
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-02-01 15:31:20 -0700

    corrected public static function profileId
    https://app.asana.com/0/256424297438823/257850751954520

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -431,4 +431,13 @@ SELECT profileId, profileImageId, profileActivation, profileEmail, profileHandle
 		}
 		return($profileId);
 	}
+
+	/**
+	 * get profile Id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @return \SplFixedArray SplFixedArray of profile id found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
 }
