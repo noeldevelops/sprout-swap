7e0ce6192f7b217b6d99e5f370d8927de8a08d0e
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-01 16:29:00 -0700

    floundering but with aim now
    https://app.asana.com/0/256424297438823/257850751954523

--- php/classes/test/MessageTest.php
+++ php/classes/test/MessageTest.php
@@ -11,9 +11,9 @@ require_once("DataDesignTest.php");
 require_once(dirname(__DIR__)) . "/autoload.php";
 
 class messageTest extends DataDesignTest{
-	protected $VALID_MESSAGEPOSTID = 7346376;
-	protected $VALID_MESSAGERECEIVERPROFILEID = 4763643;
-	protected $VALID_MESSAGESENDERPROFILEID = 345455;
+	protected $VALID_MESSAGEPOSTID = null;
+	protected $VALID_MESSAGERECEIVERPROFILEID = null;
+	protected $VALID_MESSAGESENDERPROFILEID = null;
 	protected $VALID_MESSAGEBROWSER = "Message browser passing";
 	protected $VALID_MESSAGECONTENT = "Message content passing";
 	protected $VALID_MESSAGECONTENT2 = "Message updated content passing";
@@ -21,11 +21,15 @@ class messageTest extends DataDesignTest{
 	protected $VALID_MESSAGESTATUS = 2;
 	protected $VALID_MESSAGETIMESTAMP = null;
 	/**
+	 * set up for test
 	 * creating dependent objects before running the test
 	 */
 	public final function setUp(){
 		parent::setUp();
-		$this->profile = new Profile(null, null, "ssssssssssssssss", "solomon.leyba@gmail.com", "ranch.me", null, "Solomon Leyba", );
+		$this->image = new Image(null, "sjnghsklguenghtls");
+		$this->profile = new Profile(null, null, "kjsdhkj", "solomon.leyba@gmail.com", "ranch.me", null, "Solomon Leyba", "803AE81D0D6F67C1C0D307B39A99A93F6B6499B4C6E3F2ECE96718C5E2724B96", "5A929D9C14C5DF68BD2C97BBE2652754E26B3C9D23AC91978A0B9C0EAA3DE347", "god damn i love unit testing");
+		$this->VALID_MESSAGETIMESTAMP = new \DateTime();
+
 	}
 	public function testInsertValidMessage(){
 		//store number of current rows to compare against
