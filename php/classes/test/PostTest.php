16f96d5cd9eb2a99157be77b3840aa8048e9d6c8
Noel Cothren <noelcothren@gmail.com>
2017-02-02 11:48:17 -0700

    added Unit Tests for insert, update, and delete valid and invalid Posts https://app.asana.com/0/252717071352334/257850751954519

--- php/classes/test/PostTest.php
+++ php/classes/test/PostTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Edu\Cnm\SproutSwap\Test;
-use Edu\Cnm\SproutSwap\DataDesign\{Profile, Post};
+use Edu\Cnm\SproutSwap\DataDesign\{
+	Profile, Post, Test\DataDesignTest
+};
 
 require_once("DataDesignTest.php");
 
@@ -30,14 +32,97 @@ class PostTest extends DataDesignTest {
 	 * @var int $VALID_POSTPROFILEID
 	 */
 	protected $VALID_POSTPROFILEID = null;
+
 	/**
 	 * some dependent objects to run tests with
 	 **/
 	public final function setUp() {
 		parent::setUp();
-		$this->post = new Post(null, 2, null, "browser", "content", "ipaddress", "location", "offer", "request", "2017-02-01");
-		$this->post->insert($this->getPDO());
+		//create test Profile to make a test Post
+		$this->profile = new Profile(null, null, "activation", "this@email.com", "handle", "timestamp", "My Name", "803AE81D0D6F67C1C0D307B39A99A93F6B6499B4C6E3F2ECE96718C5E2724B96", "5A929D9C14C5DF68BD2C97BBE2652754E26B3C9D23AC91978A0B9C0EAA3DE347", "This is my really cool profile.");
+		$this->profile->insert($this->getPDO());
 		$this->VALID_POSTTIMESTAMP = new \DateTime();
+	}
+
+	/**
+	 * test inserting a new Post and assert MySQL data matches
+	 */
+	public function testInsertValidPost() {
+		//count number of rows and save for later
+		$numRows = $this->getConnection()->getRowCount("post");
+		//create a new Post and insert into mySqL
+		$post = new Post(null, 2, $this->profile->getProfileId(), "browser", $this->VALID_POSTCONTENT, "IP Address", "location", "offer", "request", $this->VALID_POSTTIMESTAMP);
+		$post->insert($this->getPDO());
+		//get the mySQL data and enforce the fields match our expectations
+		$pdoPost = Post::getPostByPostId($this->getPDO(), $post->getPostId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("post"));
+		$this->assertEquals($pdoPost->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoPost->getPostContent(), $this->VALID_POSTCONTENT);
+		$this->assertEquals($pdoPost->getPostTimestamp(), $this->VALID_POSTTIMESTAMP);
+	}
+
+	/*
+	 * test inserting a Post that already exists
+	 * @expectedException PDOException
+	 */
+	public function testInsertInvalidPost() {
+		//create a post with a non-null id and make sure it fails
+		$post = new Post(DataDesignTest::INVALID_KEY, 2, $this->profile->getProfileId(), "browser", $this->VALID_POSTCONTENT, "IP Address", "location", "offer", "request", $this->VALID_POSTTIMESTAMP);
+		$post->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a post, editing it, and updating it
+	 */
+	public function testUpdateValidPost() {
+		//count number of rows and save for later
+		$numRows = $this->getConnection()->getRowCount("post");
+		//create a new Post and insert into mySqL
+		$post = new Post(null, 2, $this->profile->getProfileId(), "browser", $this->VALID_POSTCONTENT, "IP Address", "location", "offer", "request", $this->VALID_POSTTIMESTAMP);
+		$post->insert($this->getPDO());
+		//edit the post and update it
+		$post->setPostContent($this->VALID_POSTCONTENT2);
+		$post->update($this->getPDO());
+		//get the mySQL data and enforce the fields match our expectations
+		$pdoPost = Post::getPostByPostId($this->getPDO(), $post->getPostId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("post"));
+		$this->assertEquals($pdoPost->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoPost->getPostContent(), $this->VALID_POSTCONTENT2);
+		$this->assertEquals($pdoPost->getPostTimestamp(), $this->VALID_POSTTIMESTAMP);
+	}
+
+	/**
+	 * test updating a post that doesn't exist
+	 */
+	public function testUpdateInvalidPost() {
+		$post = new Post(null, 2, $this->profile->getProfileId(), "browser", $this->VALID_POSTCONTENT, "IP Address", "location", "offer", "request", $this->VALID_POSTTIMESTAMP);
+		$post->update($this->getPDO());
+	}
+	/*
+	 * test creating a post and then deleting it
+	 **/
+	public function testDeleteValidPost() {
+		//count number of rows and save for later
+		$numRows = $this->getConnection()->getRowCount("post");
+		//create a new Post and insert into mySqL
+		$post = new Post(null, 2, $this->profile->getProfileId(), "browser", $this->VALID_POSTCONTENT, "IP Address", "location", "offer", "request", $this->VALID_POSTTIMESTAMP);
+		$post->insert($this->getPDO());
+		//delete the post from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("post"));
+		$post->delete($this->getPDO());
+		//get the mySQL data and enforce the post does not exist
+		$pdoPost = Post::getPostByPostId($this->getPDO(), $post->getPostId);
+		$this->assertNull($pdoPost);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("post"));
+	}
+	/**
+	 * test deleting a post that doesn't exist
+	 * @expectedException \PDOException
+	 **/
+	public function testDeleteInvalidPost() {
+		//create a post and try to delete it without actually inserting it
+		$post = new Post(null, 2, $this->profile->getProfileId(), "browser", $this->VALID_POSTCONTENT, "IP Address", "location", "offer", "request", $this->VALID_POSTTIMESTAMP);
+		$post->delete($this->getPDO());
 	}
 
 }
