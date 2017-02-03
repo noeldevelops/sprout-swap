d98f41f55a0a39367c68ff00aa30e75cbf9ed5c3
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-02-02 21:55:09 -0700

    finished unit test mode
    https://app.asana.com/0/256424297438823/257850751954526

--- php/classes/test/ModeTest.php
+++ php/classes/test/ModeTest.php
@@ -19,12 +19,12 @@ class ModeTest extends DataDesignTest{
 	protected $VALID_MODECONTENT = "PHPUnit test passing";
 	/**
 	 * content for updated Mode
-	 * @var string $VALID_PROFILECONTENT2
+	 * @var string $VALID_MODECONTENT2
 	 **/
 	protected $VALID_MODECONTENT2 = "PHPUnit test still passing";
 	/**
 	 * timestamp of the mode; this starts as null and is assigned later
-	 * @var DateTime $VALID_PROFILEDATE
+	 * @var DateTime $VALID_MODEDATE
 	 **/
 	protected $VALID_MODEDATE = null;
 	/**
@@ -108,6 +108,104 @@ class ModeTest extends DataDesignTest{
 	 **/
 
 	public function testUpdateInvalidMode(){
-
+		//create a Mode, try to update it without actually updating it and watch it fail
+		$mode = new Mode(null, $this->mode->getModeId(), $this->VALID_MODECONTENT, $this->VALID_MODEDATE);
+		$this->update($this->getPDO());
 	}
+
+	/**
+	 * test creating a Mode and then deleting it
+	 **/
+
+	public function testDeleteValidMode(){
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("mode");
+
+		//create a new Mode and insert to into mySQL
+		$mode = new Mode(null, $this->mode->getModeId(), $this->VALID_MODECONTENT, $this->VALID_MODEDATE);
+		$mode->insert($this->getPDO());
+
+		//delete the mode from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("mode"));
+		$mode->delete($this->getPDO());
+
+		//grab the data from mySQL and enforce the Mode does not exist
+		$pdoMode = Mode::getModeByModeId($this->getPDO(), $mode->getModeId());
+		$this->assertNull($pdoMode);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("mode"));
+	}
+
+	/**
+	 * test deleting a Mode that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+
+	public function testDeleteInvalidMode(){
+		//create a Mode and try to delete it without actually inserting it
+		$mode = new Mode(null, $this->mode->getModeId(), $this->VALID_MODECONTENT, $this->VALID_MODEDATE);
+		$mode->delete($this->getPDO());
+	}
+
+	/**
+	 * test grabbing a Mode by mode content
+	 **/
+
+	public function testGetValidModeByContent(){
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("mode");
+
+		//create a new Mode and insert to into mySQL
+		$mode = new Mode(null, $this->mode->getModeId(), $this->VALID_MODECONTENT, $this->VALID_MODEDATE);
+		$mode->insert($this->getPDO());
+
+		//grab the data from mySQL and enforce the fields match our expectations
+		$results = Mode::getModeByModeContent($this->getPDO(), $mode->getModeContent());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("mode"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Zabad1\\DataDesign\\Mode", $results);
+
+		//grab the result from the array and validate it
+		$pdoMode = $results[0];
+		$this->assertEquals($pdoMode->getModeId(), $this->mode->getModeId());
+		$this->assertEquals($pdoMode->getModeContent(), $this->VALID_MODECONTENT);
+		$this->assertEquals($pdoMode->getModeDate(), $this->VALID_MODEDATE);
+	}
+
+	/**
+	 * test grabbing a Mode by content that does not exist
+	 **/
+
+	public function testGetInvalidModeByContent(){
+		//grab mode by searching for content that does not exist
+		$mode = Mode::getModeByModeContent($this->getPDO(), "you will find nothing");
+		$this->assertCount(0, $mode);
+	}
+
+	/**
+	 * test grabbing all modes
+	 **/
+
+public funcion testGetAllValidModes(){
+	//count the number of rows and save it for later
+$numRows = $this->getConnection()->getRowCount("mode");
+
+	//create a new mode and insert to into mySQL
+$mode = new Mode(null, $this->mode->getModeId(), $this->VALID_MODECONTENT, $this->VALID_MODEDATE);
+$profile->insert($this->getPDO());
+
+	//grab the data from mySQL and enforce the fields match our expectations
+$results = Mode::getAllModes($this->getPDO());
+$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("mode"));
+$this->assertCount(1, $results);
+$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Zabad1\\DataDesign\\Mode", $results);
+
+	//grab the result from the array and validate it
+$pdoMode = $results[0];
+$this->assertEquals($pdoMode->getModeId()$this->mode->getModeId());
+$this->assertEquals($pdoMode->getModeContent(), $this->VALID_MODECONTENT);
+$this->assertEquals($pdoMode->getModeDate(), $this->VALID_MODEDATE);
 }
+
+}
+?>
