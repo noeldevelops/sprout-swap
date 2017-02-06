b4eaf19f92f27436f4cb263bbb58662cca632198
Noel Cothren <noelcothren@gmail.com>
2017-02-06 14:09:09 -0700

    (still) changing DataDesign to SproutSwap

--- php/classes/test/PostTest.php
+++ php/classes/test/PostTest.php
@@ -1,6 +1,6 @@
 <?php
 namespace Edu\Cnm\SproutSwap\Test;
-use Edu\Cnm\SproutSwap\SproutSwap\{Profile, Post};
+use Edu\Cnm\SproutSwap\SproutSwapTest\{Profile, Post};
 
 require_once("SproutSwapTest.php");
 require_once(dirname(__DIR__) . "/classes/autoload.php");
