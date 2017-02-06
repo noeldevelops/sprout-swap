e20a8b020d7553278b98e4da5661cfef20d8af53
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-06 16:35:15 -0700

    created PointTest.php

---
+++ php/classes/test/PointTest.php
@@ -0,0 +1,26 @@
+<?php
+require_once("SproutSwapTest.php");
+require_once (dirname(__DIR__) . "autoload.php");
+
+/**
+ * PHPUnit test for the Point container class used by Post
+ * @see \Edu\Cnm\SproutSwap\Post
+ * @author Solomon Leyba solomon.leyba@gmail.com
+ */
+
+class PointTest extends SproutSwapTest{
+	protected $VALID_LAT = 40.9384;
+	protected $VALID_LONG = 50.2342;
+	protected $INVALID_LAT = 100.4544;
+	protected $INVALID_LONG = 200.6666;
+
+	public function testValidPoint(){
+		$point = new Point($this->VALID_LAT, $this->VALID_LONG);
+		//use mutators to make a valid case
+		$point->setLat($this->VALID_LAT);
+		$point->setLong($this->VALID_LONG);
+		//assert values are equal
+		$this->assertEquals($point->getLat(), $this->VALID_LAT);
+		$this->assertEquals($point->getLong(), $this->VALID_LONG);
+	}
+}
