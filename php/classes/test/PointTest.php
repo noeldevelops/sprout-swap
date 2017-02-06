89fbf716bf3683dad31b66c2a4a927abb790c8ae
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-06 16:37:34 -0700

    added PointTest methods

--- php/classes/test/PointTest.php
+++ php/classes/test/PointTest.php
@@ -14,6 +14,9 @@ class PointTest extends SproutSwapTest{
 	protected $INVALID_LAT = 100.4544;
 	protected $INVALID_LONG = 200.6666;
 
+	/**
+	 * test using valid coordinates
+	 */
 	public function testValidPoint(){
 		$point = new Point($this->VALID_LAT, $this->VALID_LONG);
 		//use mutators to make a valid case
@@ -22,5 +25,23 @@ class PointTest extends SproutSwapTest{
 		//assert values are equal
 		$this->assertEquals($point->getLat(), $this->VALID_LAT);
 		$this->assertEquals($point->getLong(), $this->VALID_LONG);
+	}
+	/**
+	 * test using invalid lat
+	 */
+	public function testInvalidPointLat(){
+		$point = new Point($this->INVALID_LAT, $this->VALID_LONG);
+		//use mutators to make a valid case
+		$point->setLat($this->INVALID_LAT);
+		$point->setLong($this->VALID_LONG);
+	}
+	/**
+	 * test using invalid long
+	 */
+	public function testInvalidPointLong(){
+		$point = new Point($this->VALID_LAT, $this->INVALID_LONG);
+		//use mutators to make a valid case
+		$point->setLat($this->VALID_LAT);
+		$point->setLong($this->INVALID_LONG);
 	}
 }
