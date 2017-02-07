a966f4008cdbfe8bf84a39b26d34a587739e08fd
Noel Cothren <noelcothren@gmail.com>
2017-02-07 08:48:00 -0700

    edit namespace

--- php/classes/test/SproutSwapTest.php
+++ php/classes/test/SproutSwapTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace Edu\Cnm\SproutSwap\SproutSwapTest;
+namespace Edu\Cnm\SproutSwap\Test;
 
 // grab the encrypted properties file
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
