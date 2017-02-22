2a340ed4999bbf8e041303430862ec4729d9453e
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-02-22 10:40:35 -0700

    finished create signup api
    https://app.asana.com/0/252717071352439/277452407489547

--- public_html/api/signup/index.php
+++ public_html/api/signup/index.php
@@ -46,17 +46,14 @@ try {
 
 		//ensure all required information is entered
 
-		if(empty($requestObject->profileActivation) === true) {
-			throw(new \InvalidArgumentException("Must fill in name.", 405));
-		}
 		if(empty($requestObject->profileEmail) === true) {
 			throw(new \InvalidArgumentException("Must fill in email address."));
 		}
 		if(empty($requestObject->profileHandle) === true) {
-			throw(new \InvalidArgumentException("Must fill in location."));
+			throw(new \InvalidArgumentException("Must fill in Handle"));
 		}
 		if(empty($requestObject->profileName) === true) {
-			throw(new \InvalidArgumentException("Must fill in Bio."));
+			throw(new \InvalidArgumentException("Must fill in Name."));
 		}
 		if(empty($requestObject->profilePassword) === true) {
 			throw(new \InvalidArgumentException("Must fill in password."));
@@ -69,31 +66,28 @@ try {
 		if($requestObject->profilePassword !== $requestObject->profileConfirmPassword) {
 			throw(new \InvalidArgumentException("Password does not match."));
 		}
-		// access token might not be needed here
 
 		$salt = bin2hex(random_bytes(32));
 		$hash = hash_pbkdf2("sha512", $profilePassword, $salt, 262144);
-		$profileAccessToken = bin2hex(random_bytes(16));
 		$profileActivationToken = bin2hex(random_bytes(16));
 
 		//create a new profile
 
-		$profile = new Profile(null, $requestObject->profileActivation, $requestObject->profileEmail, $requestObject->profileHandle, $requestObject->profileName, $hash, $salt, $profileAccessToken, $profileActivationToken);
+		$profile = new Profile(null,$requestObject->profileImageId, $profileActivationToken, $requestObject->profileEmail, $requestObject->profileHandle, null, $requestObject->profileName, $hash, $salt, $requestObject->profileSummary);
 		$profile->insert($pdo);
 
 //building the activation link that can travel to another server and still work. This is the link that will be clicked to confirm the account.
-//FIXME: make sure URL is /public_html/activation/$activation
 
 		$basePath = dirname($_SERVER["SCRIPT_NAME"], 2);
-		$urlglue = $basePath . "/activation/?emailActivationToken=$profileActivationToken";
+		$urlglue = $basePath . "/activation/?profileActivation=$profileActivationToken";
 		$confirmLink = "https://" . $_SERVER["SERVER_NAME"] . $urlglue;
 		$messageSubject = "Sprout-Swap Account Activation";
 		$message = <<< EOF
-<h2>Welcome to Flek!</h2>
+<h2>Welcome to Sprout-Swap!</h2>
 <p>Please visit the following URL to set a new password and complete the sign-up process: </p><p><a href="$confirmLink">$confirmLink<a></p>
 EOF;
-		$response = mailGunslinger("Flek", "gsandoval49@cnm.edu", $requestObject->profileName, $requestObject->profileEmail, $messageSubject, $message);
-		// FIXME: $response doesn't actually return "Email sent."
+		$response = mailGunslinger("Sprout-Swap", "sproutswap.team@gmail.com", $requestObject->profileName, $requestObject->profileEmail, $messageSubject, $message);
+
 		$reply->message = "Almost done! Please check your email to activate your account.";
 	} else {
 		throw(new \InvalidArgumentException("Invalid HTTP request.", 405));
