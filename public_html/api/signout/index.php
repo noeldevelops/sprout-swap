0d01325f6db1222f7a75b32946fa1a528a3b2560
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-02-23 14:07:03 -0700

    chopped use name line
    https://app.asana.com/0/252717071352439/277452407489551

--- public_html/api/signout/index.php
+++ public_html/api/signout/index.php
@@ -4,8 +4,6 @@ require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\SproutSwap\Profile;
-
 /**
  * api for Signout
  *
