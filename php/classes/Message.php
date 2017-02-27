1e7013807fdd77f2a12a8703ace5352629c5f0b8
Solomon Leyba <solomon.leyba@gmail.com>
2017-02-27 14:21:15 -0700

    rewriting update method query

--- php/classes/Message.php
+++ php/classes/Message.php
@@ -351,7 +351,7 @@ class Message implements \JsonSerializable {
 				throw (new \PDOException("cannot delete a message without an assigned messageId"));
 			}
 			//create query
-			$query = "UPDATE message WHERE messageId = $this->messageId";
+			$query = "UPDATE message SET messageStatus = :messageStatus";
 			$statement = $pdo->prepare($query);
 			//bind variables
 			$parameters = ["messageStatus" => $this->messageStatus];
