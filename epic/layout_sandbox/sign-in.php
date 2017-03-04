ef1147ef61c9e9b2cc5dabb059f4778a5243783f
Noel Cothren <noelcothren@gmail.com>
2017-03-04 11:32:11 -0700

    added sign up and sign in modal templates

---
+++ epic/layout_sandbox/sign-in.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS for Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript for Bootstrap -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<meta charset="utf-8">
+		<title>Sprout-Swap</title>
+	</head>
+	<body>
+		<div id="signin-modal">
+			<div id="logo">
+				<!--insert logo here-->
+			</div>
+			<div id="signin-text">
+				<p>Enter your information to sign in and start swapping!</p>
+			</div>
+			<form action="GET">
+				<input type="email" class="email">
+				<input type="text" id="password">
+				<button id="submit">Sign In!</button>
+			</form>
+
+		</div>
+
+	</body>
+</html>
