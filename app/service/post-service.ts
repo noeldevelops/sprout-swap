a7cc1f114010b0977088053d8c42f3daf8291bb4
Noel Cothren <noelcothren@gmail.com>
2017-03-05 12:46:50 -0700

    added post service

---
+++ app/service/post-service.ts
@@ -0,0 +1,80 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base-service";
+import {Post} from "./post-service";
+import {Status} from "../classes/status";
+import DateTimeFormat = Intl.DateTimeFormat;
+
+@Injectable()
+export class PostService extends BaseService {
+	constructor(protected http: Http) {
+		super(http);
+	}
+
+	private postUrl = "api/post/";
+
+	deletePost(postId: number) : Observable<Status> {
+		return(this.http.delete(this.postUrl + postId)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+
+	createPost(post: Post) : Observable<Status> {
+		return(this.http.post(this.postUrl, post)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+
+	editPost(post: Post) : Observable<Status> {
+		return(this.http.put(this.postUrl + post.postId, post)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+
+	getPostsByPostLocation(postLocation: any) : Observable<post[]> {
+		return(this.http.get(this.postUrl + postLocation)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getPost(postId: number) : Observable<post> {
+		return(this.http.get(this.postUrl + postId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getPostsByPostModeId(postModeId: number) : Observable<post[]> {
+		return(this.http.get(this.postUrl + postModeId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+	getPostsByPostProfileId(postProfileId: number) : Observable<post[]> {
+		return(this.http.get(this.postUrl + postProfileId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getPostsByPostContent(postContent: string) : Observable<post[]> {
+		return(this.http.get(this.postUrl + postContent)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getPostsByPostOffer(postOffer: string) : Observable<post[]> {
+		return(this.http.get(this.postUrl + postOffer)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getPostsByPostRequest(postRequest: string) : Observable<post[]> {
+		return(this.http.get(this.postUrl + postRequest)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+	getPostsByPostTimestamp(postTimestamp: DateTimeFormat) : Observable<post[]> {
+		return(this.http.get(this.postUrl + postTimestamp)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+}
