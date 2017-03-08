5f131c267751359ab41cce691d36602e3d4c9dd5
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-08 14:50:26 -0700

    image component written somewhat

---
+++ app/component/image.component.ts
@@ -0,0 +1,23 @@
+import{Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {ImageService} from "../service/image-service";
+
+import {Observable} from "rxjs/Observable"
+import {Image} from "../class/image-class";
+
+import {Status} from "../class/status";
+
+@Component({
+	templateUrl: "./templates/image-template.php"
+})
+
+export class ImageComponent implements OnInit{
+	status: Status = null;
+	post: Image = new Image(0, "");
+	constructor(
+
+	)
+	{
+	}
+
+}
