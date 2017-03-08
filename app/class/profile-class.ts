99d25702b3202504fd9b17c33411a0abdb9095d5
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-08 13:52:47 -0700

    made constuctor for profile and image classes for angular

--- app/class/profile-class.ts
+++ app/class/profile-class.ts
@@ -3,10 +3,15 @@ import DateTimeFormat = Intl.DateTimeFormat;
 export class Profile{
 	constructor(
 		public profileId: number,
-
-
+		public profileImageId: number,
+		public profileEmail: string,
+		public profileHandle: string,
+		public profileTimestamp: number,
+		public profileName: string,
+		public profilePassword: string,
+		public profileConfirmPassword: string,
+		public profileSummary: string
 	)
 	{
-
 	}
 }
