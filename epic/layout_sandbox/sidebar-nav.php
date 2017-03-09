57433d6d341cd7a95e245687eb6b06c9f9126e63
Solomon Leyba <solomon.leyba@gmail.com>
2017-03-09 16:37:41 -0700

    minor revisions to side modal nav

--- epic/layout_sandbox/sidebar-nav.php
+++ epic/layout_sandbox/sidebar-nav.php
@@ -28,8 +28,8 @@
 			<div class="col-md-1" id="modal-nav">
 				<ul id="side-nav">
 					<li><a href="#">My Profile</a></li>
-					<li><a href="#">New Post</a></li>
-					<li><a href="#">Messages</a></li>
+					<li><a href="#">My Messages</a></li>
+					<li><a href="#">Create Post</a></li>
 					<li><a href="#">Logout</a></li>
 				</ul>
 			</div>
