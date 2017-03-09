7a2415be77c719149c65cea060aeed3e9f0e9526
Noel Cothren <noelcothren@gmail.com>
2017-03-09 09:54:55 -0700

    made changes then deleted them

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -26,6 +26,7 @@ try {
 	//grab mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/sprout-swap.ini");
 
+
 	//determines which HTTP method needs to be processed and stores it in $method
 	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
