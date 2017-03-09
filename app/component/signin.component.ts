f211dcd2b201e8ee7350e38abed87c64d18f305f
Noel Cothren <noelcothren@gmail.com>
2017-03-09 16:39:26 -0700

    made a sign in component and connected to template

---
+++ app/component/signin.component.ts
@@ -0,0 +1,21 @@
+//this is the modal that pops up when "sign-in" is clicked
+
+import{Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {Observable} from "rxjs/Observable"
+import {Profile} from "../class/profile-class";
+import {Status} from "../class/status";
+
+@Component({
+	templateUrl: "./templates/signin-template.php"
+})
+
+export class SignInComponent implements OnInit{
+	status: Status = null;
+	constructor(
+
+	)
+	{
+	}
+
+}
--- public_html/templates/signin-template.php
+++ public_html/templates/signin-template.php
@@ -1,39 +1,35 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<!--this is the modal that shows when someone clicks "sign in"-->
 
-		<!-- Latest compiled and minified CSS for Bootstrap -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<div class="container">
+	<div class="col-xs-12 col-md-6" id="login-modal">
 
-		<!-- Latest compiled and minified JavaScript for Bootstrap -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
-
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-
-		<meta charset="utf-8">
-		<title>Sprout-Swap</title>
-	</head>
-	<body>
-		<div id="signin-modal">
-			<div id="logo">
-				<!--insert logo here-->
-			</div>
-			<div id="signin-text">
-				<p>Enter your information to sign in and start swapping!</p>
-			</div>
-			<form action="GET">
-				<input type="email" class="email">
-				<input type="text" id="password">
-				<button id="submit">Sign In!</button>
-			</form>
-
+		<button id="cancel">
+			<!-- closes form -->
+		</button>
+		<div class="logo">
+			<img src="../../images/sprout-swap-favi.png" alt="Welcome to Sprout-Swap!">
 		</div>
+		<!--instructions for users -->
+		<div id="login-text">
+			<p>Please enter your email and password to log-in</p>
+		</div>
+		<!--actual form-->
+		<form action="POST" id="login-form">
+			<!--user's email-->
+			<div class="formgroup">
+				<p>Email:</p>
+				<input type="email" id="signin-email">
+			</div>
+			<!--up to 75 characters; what they're giving away or selling-->
+			<div class="formgroup">
+				<p>Password:</p>
+				<input type="password" id="login-password">
+			</div>
+			<!--submit the information-->
+			<div class="formgroup" id="sign-in-final-formgroup">
+				<button id="submit">Submit</button>
+			</div>
+		</form>
 
-	</body>
-</html>
+	</div>
+</div>
