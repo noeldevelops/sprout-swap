54171b2a6044a847618b06756807133ca6d3a36d
Noel Cothren <noelcothren@gmail.com>
2017-03-09 16:56:43 -0700

    started making the profile view

--- epic/layout_sandbox/profile-view.php
+++ epic/layout_sandbox/profile-view.php
@@ -8,6 +8,8 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+		<!--Custom CSS -->
+		<link rel="stylesheet" href="../../style/public-style.css">
 		<!-- Latest compiled and minified JavaScript for Bootstrap -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
@@ -28,22 +30,23 @@
 		<main>
 			<div class="container">
 				<!--inject sidebar navigation (changes depending on if user is logged in)-->
+<div class="row">
 
 				<!--main center div-->
-				<div class="col-md-6" id="main-body">
+				<div class="col-md-6" id="main">
 					<!--profile view-->
 					<div class="profile">
 						<div class="row">
 							<div class="col-md-2">
-								<img id="profile-img">
+								<img id="profile-img" src="../../images/farmer.jpg">
 								<!--edit button is hidden unless user is signed-in and viewing their own profile-->
-								<button id="edit"></button>
-								<p id="handle"></p>
-								<p id="name"></p>
-								<p id="email"></p>
+								<button id="edit">Edit</button>
+								<p id="handle">JoeGrows</p>
+								<p id="name">Joe Mama</p>
+								<p id="email">somethign@email.com</p>
 							</div>
 							<div class="col-md-4" id="summary">
-								<p class="summary-text"></p>
+								<p class="summary-text">This is my profile. I like to grow things. I like to trade the things that I grow. I am from Albuquerque, NM and have three dogs, two cats, a horse, a cow, and some sheep.</p>
 							</div>
 						</div>
 						<div class="row">
@@ -53,6 +56,7 @@
 						</div>
 					</div>
 				</div>
+			</div>
 			</div>
 		</main>
 
