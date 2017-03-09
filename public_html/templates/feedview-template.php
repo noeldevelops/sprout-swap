9c6347ee81db1ac257ff7a902494ab9c91ed77ed
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-08 19:54:13 -0700

    added footer and feedview templates and the html code for the layout

---
+++ public_html/templates/feedview-template.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS for Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript for Bootstrap -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<meta charset="utf-8">
+		<title>Sprout-Swap</title>
+	</head>
+	<body>
+		<header id="nav">
+			<!--inject header-nav.php here-->
+		</header>
+		<main>
+			<div class="container">
+				<!--inject sidebar navigation here-->
+
+				<!--this is the main center div-->
+				<div class="col-md-6" id="main-body">
+					<!--feed view-->
+					<div class="post">
+						<img id="profile-img">
+						<p id="handle"></p>
+						<img src="#" alt="" id="post-img">
+						<p id="post-rundown"></p>
+					</div>
+				</div>
+			</div>
+		</main>
+
+	</body>
+</html>
