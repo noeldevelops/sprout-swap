8691787ee1d4172601ae06865bf6cd7a8be847f0
Solomon Leyba <solomon.leyba@gmail.com>
2017-03-09 16:00:25 -0700

    writing message class

--- app/class/message-class.ts
+++ app/class/message-class.ts
@@ -1,14 +1,11 @@
-export class Message{
-	constructor(
-		public messageId: number,
-		public messagePostId: number,
-		public messageReceiverProfileId: number,
-		public messageSenderId: number,
-		public messageContent: string,
-		public messageStatus: number,
-		public messageTimestamp: number
-	)
-	{
+export class Message {
+	constructor(public messageId: number,
+					public messagePostId: number,
+					public messageReceiverProfileId: number,
+					public messageSenderId: number,
+					public messageContent: string,
+					public messageStatus: number,
+					public messageTimestamp: number) {
 
 	}
 }
