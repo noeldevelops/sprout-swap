166a05e7dedbdbd39a2ae3d46696049a99b72bf2
Noel Cothren <noelcothren@gmail.com>
2017-03-09 16:42:41 -0700

    made a sign up component and connected to template

---
+++ app/component/signup.component.ts
@@ -0,0 +1,21 @@
+//this is the modal that pops up when "sign-up" is clicked
+
+import{Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {Observable} from "rxjs/Observable"
+import {Profile} from "../class/profile-class";
+import {Status} from "../class/status";
+
+@Component({
+	templateUrl: "./templates/signup-template.php"
+})
+
+export class SignUpComponent implements OnInit{
+	status: Status = null;
+	constructor(
+
+	)
+	{
+	}
+
+}
--- public_html/templates/signup-template.php
+++ public_html/templates/signup-template.php
@@ -1,46 +1,45 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<!--the modal that pops up when you click "sign up"-->
+<div class="container">
+	<div class="col-xs-12 col-md-6" id="login-modal">
 
-		<!-- Latest compiled and minified CSS for Bootstrap -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- Latest compiled and minified JavaScript for Bootstrap -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
-
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-
-		<meta charset="utf-8">
-		<title>Sprout-Swap</title>
-	</head>
-	<body>
-		<div id="signup-modal">
-			<div id="logo">
-				<!--insert logo here-->
-			</div>
-			<div id="signup-text">
-				<p>Sign up for Sprout Swap and start trading veggies today!</p>
-			</div>
-			<form action="POST">
-				<input type="text" id="name">
-				<input type="text" id="handle">
-				<input type="email" class="email">
-				<input type="text" id="password">
-				<input type="text" id="confirm-pass">
-				<button id="submit">Sign Up!</button>
-			</form>
-			<!--hidden div that shows after valid form submitted-->
-			<div id="activate">
-				<p>Almost done! check your email to confirm. Please allow ample time for the email to arrive.</p>
-			</div>
-
+		<button id="cancel">
+			<!-- closes form -->
+		</button>
+		<div class="logo">
+			<img src="../../images/sprout-swap-favi.png" alt="Welcome to Sprout-Swap!">
+		</div>
+		<!--instructions for users -->
+		<div id="login-text">
+			<p>Please enter your information to sign-up for Sprout-Swap</p>
 		</div>
 
-	</body>
-</html>
+		<!--actual form-->
+		<form action="POST" id="signup-form">
+			<!--user's email-->
+			<div class="formgroup">
+				<p>Email:</p>
+				<input type="email" id="signup-email">
+			</div>
+			<!--user enters password-->
+			<div class="formgroup">
+				<p>Password:</p>
+				<input type="password" id="signup-password">
+			</div>
+			<!--confirm password-->
+			<div class="formgroup">
+				<p>Confirm Password:</p>
+				<input type="password" id="signup-password-confirm">
+			</div>
+			<!--set a handle-->
+			<div class="formgroup">
+				<p>Choose a unique username:</p>
+				<input type="text" id="signup-handle">
+			</div>
+			<!--submit the information-->
+			<div class="formgroup" id="sign-in-final-formgroup">
+				<button id="submit">Submit</button>
+			</div>
+		</form>
+
+	</div>
+</div>
