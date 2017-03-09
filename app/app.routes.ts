1478a0c74d48566289cc1af13d6678e99fbdec28
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-08 17:10:10 -0700

    added paths to my routes

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,16 +2,18 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./component/home-component";
 import {SignUpComponent} from "./component/signup-component";
 import {ProfileComponent} from "./component/profile.component";
+import {MessageComponent} from "./component/message.component";
+import {PostComponent} from "./component/newpost.component";
 
 
-export const allAppComponents = [HomeComponent, SignUpComponent, ProfileComponent];
+export const allAppComponents = [HomeComponent, SignUpComponent, ProfileComponent, MessageComponent, PostComponent, NotFoundComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
 	{path: "profile", component: ProfileComponent},
 	{path: "message", component: MessageComponent},
 	{path: "post", component: PostComponent},
-	{path: "**", component: NotFound}
+	{path: "**", component: NotFoundComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
