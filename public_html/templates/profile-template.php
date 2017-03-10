b3b2a36edf85fce782c59f5eb89a4fb06193f14b
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-10 10:45:02 -0700

    updated profile-template

--- public_html/templates/profile-template.php
+++ public_html/templates/profile-template.php
@@ -1,60 +1,21 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-		<!-- Latest compiled and minified CSS for Bootstrap -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- Latest compiled and minified JavaScript for Bootstrap -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
-
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-
-		<meta charset="utf-8">
-		<title>Sprout-Swap</title>
-	</head>
-	<body>
-		<header>
-			<div class="container">
-				<!--inject our header/logo and navbar-->
-			</div>
-		</header>
-		<main>
-			<div class="container">
-				<!--inject sidebar navigation (changes depending on if user is logged in)-->
-
-				<!--main center div-->
-				<div class="col-md-6" id="main-body">
-					<!--profile view-->
-					<div class="profile">
-						<div class="row">
-							<div class="col-md-2">
-								<img id="profile-img">
-								<!--edit button is hidden unless user is signed-in and viewing their own profile-->
-								<button id="edit"></button>
-								<p id="handle"></p>
-								<p id="name"></p>
-								<p id="email"></p>
-							</div>
-							<div class="col-md-4" id="summary">
-								<p class="summary-text"></p>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-6" id="profile-history">
-								<!-- inject posts from this user, with edit buttons if user is logged in like above -->
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</main>
-
-	</body>
-</html>
+<div class="col-md-6" id="profile">
+	<div id="img-wrap">
+		<img id="profile-img" src="../../images/farmer.jpg">
+		<button id="edit">Edit Profile</button>
+	</div>
+	<div id="profile-left">
+		<!--edit button is hidden unless user is signed-in and viewing their own profile-->
+		<p id="profile-handle">{{profile-handle}}: JoeGrows</p>
+		<p id="profile-name">{{profile-name}}: Joe Mama</p>
+		<p id="profile-email">{{profile-email}}: somethign@email.com</p>
+	</div>
+	<div id="profile-right">
+		<p id="summary-text">{{profile-summary}}: This is my profile. I like to grow things. I like to trade
+			the things
+			that I grow. I am from Albuquerque, NM and have three dogs, two cats, a horse, a cow, and
+			some sheep.</p>
+	</div>
+</div>
+<div id="profile-post-view">
+	<!-- will show all posts from a user or placeholder if they have made none-->
+</div>
