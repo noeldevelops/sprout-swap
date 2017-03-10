9a818413036fc718e42a9fc395906dc4ef6a8f59
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-10 11:47:41 -0700

    updated app css file an d profile template

--- public_html/templates/profile-template.php
+++ public_html/templates/profile-template.php
@@ -1,21 +1,26 @@
-<div class="col-md-6" id="profile">
-	<div id="img-wrap">
-		<img id="profile-img" src="../../images/farmer.jpg">
+<div class="col-md-6 col-xs-12" id="profile">
+	<div class="row">
+		<div id="img-wrap">
+			<img id="profile-img" src="../../images/farmer.jpg">
+		</div>
 		<button id="edit">Edit Profile</button>
 	</div>
-	<div id="profile-left">
-		<!--edit button is hidden unless user is signed-in and viewing their own profile-->
-		<p id="profile-handle">{{profile-handle}}: JoeGrows</p>
-		<p id="profile-name">{{profile-name}}: Joe Mama</p>
-		<p id="profile-email">{{profile-email}}: somethign@email.com</p>
+	<div class="row">
+		<div id="profile-left">
+			<!--edit button is hidden unless user is signed-in and viewing their own profile-->
+			<p id="profile-handle">{{profile-handle}}: JoeGrows</p>
+			<p id="profile-name">{{profile-name}}: Joe Mama</p>
+			<p id="profile-email">{{profile-email}}: somethign@email.com</p>
+		</div>
+		<div id="profile-right">
+			<p id="summary-text">{{profile-summary}}: This is my profile. I like to grow things. I like to
+				trade
+				the things
+				that I grow. I am from Albuquerque, NM and have three dogs, two cats, a horse, a cow, and
+				some sheep.</p>
+		</div>
 	</div>
-	<div id="profile-right">
-		<p id="summary-text">{{profile-summary}}: This is my profile. I like to grow things. I like to trade
-			the things
-			that I grow. I am from Albuquerque, NM and have three dogs, two cats, a horse, a cow, and
-			some sheep.</p>
+	<div id="profile-post-view">
+		<!-- will show all posts from a user or placeholder if they have made none-->
 	</div>
-</div>
-<div id="profile-post-view">
-	<!-- will show all posts from a user or placeholder if they have made none-->
 </div>
