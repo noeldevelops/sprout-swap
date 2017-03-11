8a9cc38123b28f4faee32f1df82a8f5e9ddb567c
Noel Cothren <noelcothren@gmail.com>
2017-03-11 16:49:00 -0700

    import services in app.module

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,16 +6,23 @@ import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 import {SignInService} from "./service/signin-service";
 import {SignUpService} from "./service/signup-service";
+import {ProfileService} from "./service/profile-service";
+import {PostService} from "./service/post-service";
+import {ModeService} from "./service/mode-service";
+import {ImageService} from "./service/image-service";
+import {BaseService} from "./service/base-service";
+import {MessageService} from "./service/message-service";
+import {SignOutService} from "./service/signout-service";
 
 const moduleDeclarations = [AppComponent];
 
 
 @NgModule({
 	imports: [BrowserModule, FormsModule, HttpModule, routing],
-	//tells angular which componenents/ must declare every component, directive, and pipe
+	//tells angular which components/ must declare every component, directive, and pipe
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap: [AppComponent],
-	providers: [appRoutingProviders, SignInService, SignUpService]
+	providers: [appRoutingProviders, SignInService, SignUpService, SignOutService, ProfileService, PostService, ModeService, ImageService, MessageService]
 })
 
 export class AppModule {}
