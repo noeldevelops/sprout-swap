5f442958d6b4a4833ef2d6e8aa2b00634c5aaf40
Noel Cothren <noelcothren@gmail.com>
2017-03-11 13:20:12 -0700

    added selector

--- app/component/newmessage.component.ts
+++ app/component/newmessage.component.ts
@@ -1,16 +1,14 @@
 import{Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 import {MessageService} from "../service/message-service";
-
 import {Observable} from "rxjs/Observable"
 import {Message} from "../class/message-class";
-
 import {Profile} from "../class/profile-class";
-
 import {Status} from "../class/status";
 
 @Component({
-	templateUrl: "./templates/newmessage-template.php"
+	templateUrl: "./templates/newmessage-template.php",
+	selector: 'new-message'
 })
 
 export class NewMessageComponent implements OnInit{
