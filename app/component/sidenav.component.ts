a0e060d326d148a2c49dbcc3117cbd64c162343c
Noel Cothren <noelcothren@gmail.com>
2017-03-11 12:39:55 -0700

    added sign out method and ngOnInit to sidenav

--- app/component/sidenav.component.ts
+++ app/component/sidenav.component.ts
@@ -4,17 +4,26 @@ import {Router} from "@angular/router";
 import {Observable} from "rxjs/Observable"
 
 import {Status} from "../class/status";
+import {SignOutService} from "../service/signout-service";
 
 @Component({
-	templateUrl: "./templates/sidenav-template.php"
+	templateUrl: "./templates/sidenav-template.php",
+	selector: 'sidenav'
 })
 
-export class SideNavComponent {
+export class SideNavComponent implements OnInit{
 	status: Status = null;
-	constructor(
+	constructor(private SignOutService: SignOutService, private router: Router) {}
 
-	)
-	{
+	ngOnInit() : void {}
+
+	signOut() : void {
+		this.SignOutService.getSignOut()
+			.subscribe(status => {
+				this.status = status;
+				if(status.status === 200) {
+					this.router.navigate(['']);
+				}
+			});
 	}
-
 }
