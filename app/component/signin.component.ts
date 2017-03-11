462036d83545080d4b84c8188615ead1adbdb451
Noel Cothren <noelcothren@gmail.com>
2017-03-11 10:33:43 -0700

    fixing sign in component

--- app/component/signin.component.ts
+++ app/component/signin.component.ts
@@ -3,10 +3,9 @@
 import{Component, ViewChild, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 import {Observable} from "rxjs/Observable"
-import {Profile} from "../class/profile-class";
 import {Status} from "../class/status";
 import {SignInService} from "../service/signin-service";
-import {ProfileService} from "../service/profile-service";
+import {SignIn} from "../class/signin-class";
 declare var $: any;
 
 @Component({
@@ -17,23 +16,19 @@ declare var $: any;
 export class SignInComponent implements OnInit {
 	@ViewChild("signin-form") signInForm : any;
 
-	profile: Profile = new Profile(0, 0, "", "", 0, "","", "", "");
+	signin: SignIn = new SignIn("", "");
 	status: Status = null;
-	constructor(private SignInService: SignInService, private profileService: ProfileService, private router: Router){}
+
+	constructor(private SignInService: SignInService, private router: Router){}
 
 	ngOnInit(): void {}
 
 	signIn() : void {
-		this.SignInService.postSignIn(this.profile)
+		this.SignInService.postSignIn(this.signin)
 			.subscribe(status => {
 				this.status = status;
 				if(status.status === 200) {
 					this.router.navigate(['main-app']);
-					this.signInForm.reset()
-					// this.SignInService.isLoggedIn = true;
-					setTimeout(function() {
-						$("#signin-modal").modal('hide');
-					}, 1000);
 				}
 			});
 	}
