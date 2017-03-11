208165cf61b4df4150b09582931ed5fc972e5c0f
Noel Cothren <noelcothren@gmail.com>
2017-03-11 16:49:26 -0700

    make p tags into labels and add form id's

--- public_html/templates/signup-template.php
+++ public_html/templates/signup-template.php
@@ -6,30 +6,30 @@
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
 						aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title">Enter your information to sign-up for Sprout-Swap!</h4>
+				<h4 class="modal-title">Enter your information to Sign Up for Sprout-Swap!</h4>
 			</div>
 
 			<!--actual form-->
 			<form #signupForm="ngForm" name="signupForm" (ngSubmit)="createProfile();">
 				<!--user's email-->
 				<div class="formgroup">
-					<p class="modal-labels">Email:</p>
-					<input type="email" name ="email" class="modal-inputs" required [(ngModel)]="profile.profileEmail" #profileEmail="ngModel">
+					<label for="email" class="modal-labels">Email:</label>
+					<input type="email" id="email" name ="email" class="modal-inputs" required [(ngModel)]="profile.profileEmail" #profileEmail="ngModel">
 				</div>
 				<!--user enters password-->
 				<div class="formgroup">
-					<p class="modal-labels">Password:</p>
-					<input type="password" name="password" class="modal-inputs" required [(ngModel)]="profile.profilePassword" #profilePassword="ngModel">
+					<label for="password" class="modal-labels">Password:</label>
+					<input type="password" id="password" name="password" class="modal-inputs" required [(ngModel)]="profile.profilePassword" #profilePassword="ngModel">
 				</div>
 				<!--confirm password-->
 				<div class="formgroup">
-					<p class="modal-labels">Confirm Password:</p>
-					<input type="password" name="confirmPassword" class="modal-inputs" required [(ngModel)]="profile.profileConfirmPassword" #profileConfirmPassword="ngModel">
+					<label for="confirmPassword" class="modal-labels">Confirm Password:</label>
+					<input type="password" id="confirmPassword" name="confirmPassword" class="modal-inputs" required [(ngModel)]="profile.profileConfirmPassword" #profileConfirmPassword="ngModel">
 				</div>
 				<!--set a handle-->
 				<div class="formgroup">
-					<p class="modal-labels">Choose a unique username:</p>
-					<input type="text" name="handle" class="modal-inputs" required [(ngModel)]="profile.profileHandle" #profileHandle="ngModel">
+					<input for="handle" class="modal-labels">Choose a unique username:</input>
+					<input type="text" id="handle" name="handle" class="modal-inputs" required [(ngModel)]="profile.profileHandle" #profileHandle="ngModel">
 				</div>
 				<!--submit the information-->
 				<input type="submit" name="signup" class="modal-submit" value="signup">
@@ -41,3 +41,4 @@
 			</form>
 		</div>
 	</div>
+</div>
