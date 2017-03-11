af72dfe1c133eb4601dcb896d21fda24f83b38b6
Solomon Leyba <solomon.leyba@gmail.com>
2017-03-11 12:56:50 -0700

    finishing new message modal

--- epic/layout_sandbox/new-message.php
+++ epic/layout_sandbox/new-message.php
@@ -8,48 +8,51 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-		<!--Custom CSS -->
-		<link rel="stylesheet" href="../../style/public-style.css">
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
 		<!-- Latest compiled and minified JavaScript for Bootstrap -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
 
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!--Custom CSS -->
+		<link rel="stylesheet" href="../../style/public-style.css">
 
 		<meta charset="utf-8">
 		<title>Sprout-Swap</title>
 	</head>
 
-	<div class="container">
-		<div class="col-xs-12 col-md-6" id="new-message-modal">
+	<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm">Sign In</button>
 
-			<button id="cancel">
-				<!-- closes form -->
-			</button>
-
-			<!--instructions for users -->
-			<div id="new-message-text">
-				<p>Enter your message below</p>
-			</div>
-			<!--actual form-->
-			<form action="POST" id="new-message-form">
-				<!--an image of the produce they are offering-->
-				<div class="formgroup">
-					<p>{{messageSender}} to {{messageReceiver}}</p>
+	<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+		<div class="modal-dialog modal-sm" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+							aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title">Send a message!</h4>
 				</div>
-				<!--allows users to write in further details about what they're offering -->
-				<div class="formgroup">
-					<textarea class="form-control" id="message-content" rows="5" name="message-content" placeholder="Enter your message here (Max 500 characters)">
+
+				<!--actual form-->
+				<form action="POST" id="message-form">
+					<!-- shows users who they're sending to NOTE: message receiver cannot be filled in by user they must get to this page by slicking on a button from either a post or a profile page; this will auto-populate the receiver field -->
+					<div class="formgroup">
+						<p class="modal-labels">{{messageSender}} to {{messageReceiver}}</p>
+					</div>
+					<!--actual message-->
+					<div class="formgroup">
+					<textarea class="form-control modal-inputs" id="message-content" rows="5" name="message-content"
+								 placeholder="Enter your message here (Max 500 characters)">
 					</textarea>
-				</div>
-				<!--submit the information-->
-				<div class="formgroup" id="new-message-final-formgroup">
-					<button id="submit">Submit</button>
-				</div>
-			</form>
-
+					</div>
+					<!--submit the information-->
+					<div class="formgroup" id="signin-final-formgroup">
+						<button class="modal-submit">Submit</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
 </html>
