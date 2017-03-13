411ba538d01fc3b6676d59be7a2845802b7660d8
Solomon Leyba <solomon.leyba@gmail.com>
2017-03-13 09:25:57 -0600

    deleting placeholder text on profile view

--- public_html/templates/profile-template.php
+++ public_html/templates/profile-template.php
@@ -10,16 +10,12 @@
 	<div class="row">
 		<div id="profile-left">
 			<!--edit button is hidden unless user is signed-in and viewing their own profile-->
-			<p id="profile-handle">{{ profile.profileHandle }}: JoeGrows</p>
-			<p id="profile-name">{{ profile.profileName }}: Joe Mama</p>
-			<p id="profile-email">{{ profile.profileEmail }}: somethign@email.com</p>
+			<p id="profile-handle">{{ profile.profileHandle }}</p>
+			<p id="profile-name">{{ profile.profileName }}</p>
+			<p id="profile-email">{{ profile.profileEmail }}</p>
 		</div>
 		<div id="profile-right">
-			<p id="summary-text">{{ profile.profileSummary }}: This is my profile. I like to grow things. I like to
-				trade
-				the things
-				that I grow. I am from Albuquerque, NM and have three dogs, two cats, a horse, a cow, and
-				some sheep.</p>
+			<p id="summary-text">{{ profile.profileSummary }}</p>
 		</div>
 	</div>
 	<!--	<div id="profile-post-view">-->
