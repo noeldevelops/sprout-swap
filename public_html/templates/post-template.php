fc46764a3fe1146250a9e62779c084a73cc866c9
Solomon Leyba <solomon.leyba@gmail.com>
2017-03-13 09:58:43 -0600

    more work on post-templating

--- public_html/templates/post-template.php
+++ public_html/templates/post-template.php
@@ -1,15 +1,13 @@
-<!--the main view of a single post (it will populate the post feed and the new post modal)-->
-<!--todo delete fake images and words-->
-
+<!--the main view of a single post (it will populate the post feed)-->
 <div class="row main">
 	<div class="col-md-6 display">
 		<div class="row post">
 			<div class="col-md-3">
-				<img src="../../images/farmer.jpg" id="profile-img">
+				<img src="../../images/farmer.jpg" id="profile-img"/>
 				<p id="handle">{{ post.postProfileId }}</p>
 			</div>
 			<div class="col-md-9">
-				<img src="../../images/zuccs.jpg" alt="" id="post-img">
+				<img src="../../images/zuccs.jpg" alt="" id="post-img"/>
 				<p class="post-rundown">{{ post.postOffer }}</p>
 				<p class="post-rundown">{{ post.postModeId }}</p>
 				<p class="post-rundown">{{ post.postRequest }}</p>
