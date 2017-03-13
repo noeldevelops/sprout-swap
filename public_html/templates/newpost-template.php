0ec03514b312d75c3c5cda732d1e27e0e5354833
Solomon Leyba <solomon.leyba@gmail.com>
2017-03-13 10:21:08 -0600

    new post template edits

--- public_html/templates/newpost-template.php
+++ public_html/templates/newpost-template.php
@@ -1,50 +1,49 @@
-<div class="container">
-	<div class="col-xs-12 col-md-6" id="new-post-modal">
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#newPostModel">New Post</button>
 
-		<button id="cancel-post">
-			<!-- closes form -->
-		</button>
-		<div class="logo">
-			<img src="../../images/sprout-swap-favi.png" alt="Welcome to Sprout-Swap!">
-		</div>
-		<!--instructions for users -->
-		<div id="new-post-text">
-			<p>Make a post and start trading!</p>
-		</div>
+<div class="modal fade" tabindex="-1" role="dialog" id="newPostModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+						aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Enter your information to Sign Up for Sprout-Swap!</h4>
+			</div>
 
-		<!--actual form-->
-		<form #newPostForm="ngForm" name="newPostForm" (ngSubmit)="createPost();">
-			<!--an image of the produce they are offering-->
-			<div class="formgroup">
-				<p>Upload an image of your produce</p>
-				<input type="file" name="postImage" id="postImage" required [(ngModel)]="post.postImageId" #profileEmail="ngModel">
-			</div>
-			<!--up to 75 characters; what they're giving away or selling-->
-			<div class="formgroup">
-				<p>Tell us what you have for offer</p>
-				<input type="text" id="postOffer" required [(ngModel)]="post.postOffer" #postOffer="ngModel">
-			</div>
-			<!--the mode: for free, for sale, or for trade-->
-			<div class="formgroup">
-				<p>Is your produce free, for sale, or are you looking to trade?</p>
-				<select id="postMode" name="postMode" required [(ngModel)]="post.postModeId" #postMode="ngModel">
-					<option value="1">Free</option>
-					<option value="2">Sell</option>
-					<option value="3">Trade</option>
-				</select>
-			</div>
-			<!--what they are looking for: cash or other veggies.  only shows up if selected mode is not 'free' -->
-			<div class="formgroup">
-				<p>What do you want in return?</p>
-				<input type="text" name="postRequest" id="postRequest" required [(ngModel)]="post.postRequest" #postRequest="ngModel">
-			</div>
-			<!--allows users to write in further details about what they're offering -->
-			<div class="formgroup">
-				<p>Give more details about your produce!</p>
-				<textarea class="form-control" id="postSummary" rows="5" name="postSummary" placeholder="Max 500 characters" required [(ngModel)]="post.postSummary" #postSummary="ngModel"></textarea>
-			</div>
-			<!--submit the information-->
-			<input type="submit" name="newPost" id="newPost" class="modal-submit" value="Submit">
-		</form>
+			<!--	begin actual form -->
+			<form #newPostForm="ngForm" name="newPostForm" (ngSubmit)="createPost();">
+				<!--an image of the produce they are offering-->
+				<div class="form-group">
+					<p>Upload an image of your produce</p>
+					<input type="file" name="postImage" id="postImage" class="modal-inputs" required [(ngModel)]="post.postImageId" #postImage="ngModel">
+				</div>
+				<!--up to 75 characters; what they're giving away or selling-->
+				<div class="form-group">
+					<p class="modal-labels">Tell us what you have for offer</p>
+					<input type="text" id="postOffer" class="modal-inputs" required [(ngModel)]="post.postOffer" #postOffer="ngModel">
+				</div>
+				<!--the mode: for free, for sale, or for trade-->
+				<div class="form-group">
+					<p class="modal-labels">Is your produce free, for sale, or are you looking to trade?</p>
+					<select id="postMode" name="postMode" class="modal-inputs" required [(ngModel)]="post.postModeId" #postMode="ngModel">
+						<option value="1">Free</option>
+						<option value="2">Sell</option>
+						<option value="3">Trade</option>
+					</select>
+				</div>
+				<!--what they are looking for: cash or other veggies.  only shows up if selected mode is not 'free' -->
+				<div class="form-group">
+					<p class="modal-labels"> What do you want in return?</p>
+					<input type="text" name="postRequest" id="postRequest" class="modal-inputs" required [(ngModel)]="post.postRequest" #postRequest="ngModel">
+				</div>
+				<!--allows users to write in further details about what they're offering -->
+				<div class="form-group">
+					<p class="modal-labels">Give more details about your produce!</p>
+					<textarea class="form-control modal-inputs" id="postSummary" rows="5" name="postSummary" placeholder="Max 500 characters" required [(ngModel)]="post.postSummary" #postSummary="ngModel"></textarea>
+				</div>
+				<!--submit the information-->
+				<input type="submit" name="newPost" id="newPost" class="modal-submit" value="Submit">
+				<!--<div>{{status.message}}</div>-->
+			</form>
+		</div>
 	</div>
 </div>
