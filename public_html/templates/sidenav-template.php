df2b7969dd24089479d5ec19ef8478a389bc4fa2
Noel Cothren <noelcothren@gmail.com>
2017-03-13 16:20:43 -0600

    move a router link to the side nav

--- public_html/templates/sidenav-template.php
+++ public_html/templates/sidenav-template.php
@@ -2,7 +2,7 @@
 <div class="container">
 	<div class="col-md-1" id="modal-nav">
 		<ul id="side-nav">
-			<li><a href="#">My Profile</a></li>
+			<li><a routerLink="/profile/:id">My Profile</a></li>
 			<li><a href="#">My Messages</a></li>
 			<li><a href="#">Create Post</a></li>
 			<li><a href="#">Logout</a></li>
