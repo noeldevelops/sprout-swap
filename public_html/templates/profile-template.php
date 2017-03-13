6747d931ecd1c9af37864f31fcdb9d6019b505ff
Solomon Leyba <solomon.leyba@gmail.com>
2017-03-13 09:24:44 -0600

    editing profile template

--- public_html/templates/profile-template.php
+++ public_html/templates/profile-template.php
@@ -3,24 +3,26 @@
 		<div id="img-wrap">
 			<img id="profile-img" src="../../images/farmer.jpg">
 		</div>
-		<button id="edit">Edit Profile</button>
+		<div *ngIf="profile.profileId === <?php $_SESSION['profile']->getProfileId() ?>">
+			<button id="edit">Edit Profile</button>
+		</div>
 	</div>
 	<div class="row">
 		<div id="profile-left">
 			<!--edit button is hidden unless user is signed-in and viewing their own profile-->
-			<p id="profile-handle">{{profile-handle}}: JoeGrows</p>
-			<p id="profile-name">{{profile-name}}: Joe Mama</p>
-			<p id="profile-email">{{profile-email}}: somethign@email.com</p>
+			<p id="profile-handle">{{ profile.profileHandle }}: JoeGrows</p>
+			<p id="profile-name">{{ profile.profileName }}: Joe Mama</p>
+			<p id="profile-email">{{ profile.profileEmail }}: somethign@email.com</p>
 		</div>
 		<div id="profile-right">
-			<p id="summary-text">{{profile-summary}}: This is my profile. I like to grow things. I like to
+			<p id="summary-text">{{ profile.profileSummary }}: This is my profile. I like to grow things. I like to
 				trade
 				the things
 				that I grow. I am from Albuquerque, NM and have three dogs, two cats, a horse, a cow, and
 				some sheep.</p>
 		</div>
 	</div>
-	<div id="profile-post-view">
-		<!-- will show all posts from a user or placeholder if they have made none-->
-	</div>
+	<!--	<div id="profile-post-view">-->
+	<!--		<!-- will show all posts from a user or placeholder if they have made none-->-->
+	<!--	</div>-->
 </div>
