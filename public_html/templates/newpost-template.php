6fc72e3a7d83e33625f39bfe240fa06101a6c1df
Noel Cothren <noelcothren@gmail.com>
2017-03-14 08:37:51 -0600

    make a file upload template

--- public_html/templates/newpost-template.php
+++ public_html/templates/newpost-template.php
@@ -15,7 +15,7 @@
 				<!--an image of the produce they are offering-->
 				<div class="form-group">
 					<label for="postImage" class="modal-labels">Upload an image</label>
-					<input type="file" ng2FileSelect [uploader]="uploader" name="postImage" id="postImage" class="modal-inputs"
+					 <input type="file" name="postImage" id="postImage" class="modal-inputs"
 							 [(ngModel)]="newpost.postImageId" #postImageId="ngModel">
 				</div>
 				<!--up to 75 characters; what they're giving away or selling-->
