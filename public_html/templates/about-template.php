42da7b2279c31fcdd254f61d9cc6b5369677d2e8
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-14 17:57:12 -0600

    about us file created

---
+++ public_html/templates/about-template.php
@@ -0,0 +1,59 @@
+<header class="template-header">
+	<div class="col-12 text-center">
+		<a routerLink=""><img src="images/logosmaller.png"></a>
+	</div>
+</header>
+<!-- About Section -->
+<section id="about-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2 class="section-heading">About This Project</h2><br>
+				<div class="about">
+					<p><strong>Sprout-Swap</strong> Sprout Swap is a social site that provides an outlet to trade, sell, or give away produce that users grow. This site helps people make a connection with others that grow their own produce as a hobby. The idea came from a friend who grows her own produce and often has an abundance of one veggie or another. She wanted a way to make sure her surplus produce didn’t go to waste, either by giving it away to people who would eat it, or trading with other growers and promoting the hobby farming community. Sprout-Swap is a hub to help people make connections with other growers. </p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Team Section -->
+<section id="team-section" class="bg-light-gray">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2 class="section-heading">Our Team</h2>
+			</div>
+		</div>
+	</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="team-member">
+					<img src="" class="img-responsive img-circle" alt="">
+					<h4>Noel Cothren</h4>
+						<p></p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="team-member">
+					<img src="" class="img-responsive img-circle" alt="">
+					<h4>Solomon Leyba</h4>
+						<p></p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="team-member">
+					<img src="" class="" alt="">
+					<h4>Zak Abad</h4>
+						<p></p>
+				</div>
+			</div>
+		</div>
+</section>
