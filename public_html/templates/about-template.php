8c34c8b1006dfebf46678436295bde29d5d59913
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-03-15 14:07:27 -0600

    center new post button

--- public_html/templates/about-template.php
+++ public_html/templates/about-template.php
@@ -1,8 +1,3 @@
-<header class="template-header">
-	<div class="col-12 text-center">
-		<a routerLink=""><img src="images/logosmaller.png"></a>
-	</div>
-</header>
 <!-- About Section -->
 <section id="about-section">
 	<div class="container">
@@ -10,7 +5,7 @@
 			<div class="col-lg-12 text-center">
 				<h2 class="section-heading">About This Project</h2><br>
 				<div class="about">
-					<p><strong>Sprout-Swap</strong> Sprout Swap is a social site that provides an outlet to trade, sell, or give away produce that users grow. This site helps people make a connection with others that grow their own produce as a hobby. The idea came from a friend who grows her own produce and often has an abundance of one veggie or another. She wanted a way to make sure her surplus produce didn’t go to waste, either by giving it away to people who would eat it, or trading with other growers and promoting the hobby farming community. Sprout-Swap is a hub to help people make connections with other growers. </p>
+					<p><strong>Sprout-Swap</strong> is a social site that provides an outlet to trade, sell, or give away produce that users grow. This site helps people make a connection with others that grow their own produce as a hobby. The idea came from a friend who grows her own produce and often has an abundance of one veggie or another. She wanted a way to make sure her surplus produce didn’t go to waste, either by giving it away to people who would eat it, or trading with other growers and promoting the hobby farming community. Sprout-Swap is a hub to help people make connections with other growers. </p>
 				</div>
 			</div>
 		</div>
@@ -50,10 +45,10 @@
 		<div class="row">
 			<div class="col-md-6">
 				<div class="team-member">
-					<img src="" class="" alt="">
+					<img src="/images/IMG_2033.JPG" id="rotate" class="img-responsive img-circle" alt="">
 					<h4>Zak Abad</h4>
 						<p></p>
 				</div>
 			</div>
-		</div>
+	</div>
 </section>
