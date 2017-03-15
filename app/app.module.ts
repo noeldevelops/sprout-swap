04120cc254dc5b0943943f7d71d60061cf6a318b
Solomon Leyba <solomon.leyba@gmail.com>
2017-03-15 13:09:30 -0600

    importing modules for ngif support

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {CommonModule} from "@angular/common";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 import {SignInService} from "./service/signin-service";
@@ -19,7 +20,7 @@ const moduleDeclarations = [AppComponent];
 
 
 @NgModule({
-	imports: [BrowserModule, FormsModule, HttpModule, routing],
+	imports: [BrowserModule, FormsModule, HttpModule, routing, CommonModule],
 	//tells angular which components/ must declare every component, directive, and pipe
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap: [AppComponent],
